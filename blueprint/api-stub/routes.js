38b3dd24598606b660e540627ef99c49f29d610a
Eric Kelly <heroiceric@gmail.com>
2014-03-22 12:23:51 -0400

    Fix indentation
    Uses 2 spaces instead of tabs

--- blueprint/api-stub/routes.js
+++ blueprint/api-stub/routes.js
@@ -3,10 +3,10 @@ module.exports = function(server) {
 
   // Create an API namespace, so that the root does not 
   // have to be repeated for each end point.
-	server.namespace('/api', function() {
+  server.namespace('/api', function() {
 
-		// Return fixture data for '/api/posts/:id'
-		server.get('/posts/:id', function(req, res) {
+    // Return fixture data for '/api/posts/:id'
+    server.get('/posts/:id', function(req, res) {
       var post = {
         "post": {
           "id": 1,
@@ -24,7 +24,7 @@ module.exports = function(server) {
         }]
       };
 
-			res.send(post);
-		});
-	});
+      res.send(post);
+    });
+  });
 };
