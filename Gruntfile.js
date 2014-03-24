255a842f60e8fa9c2a713206a44f0bbfd84b0ced
Joe Fiorini <joe@joefiorini.com>
2014-03-24 00:43:57 -0400

    Configure grunt-release plugin

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var npm = require('npm');
+
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-release');
+
+  grunt.initConfig({
+    release: {
+      options: {
+        tagName: 'v<%= version %>',
+        commitMessage: 'bump v<%= version %> :tada:',
+        tagMessage: 'v<%= version %>'
+      }
+    }
+  });
+
+  grunt.registerTask('test', function() {
+    var done = this.async();
+
+    npm.load({
+      logLevel: 'error'
+    }, function(error) {
+      if(error) {
+        grunt.fail.fatal('Could not load npm');
+        return;
+      }
+
+      npm.runScript('test', function(error) {
+        if(error) {
+          grunt.fail.fatal('tests failed');
+          return;
+        }
+
+        grunt.log.write('tests passed, wtg');
+        done();
+      });
+    });
+  });
+
+  grunt.registerTask('ember:release', ['test', 'release']);
+  for(var level in ['patch', 'major', 'minor']) {
+    grunt.registerTask('ember:release:' + level, ['test', 'release:' + level]);
+  }
+
+};
