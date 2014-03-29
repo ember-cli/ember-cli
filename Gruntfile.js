acb088c14963598b6472ec50992f7177194e8320
Stefan Penner <stefan.penner@gmail.com>
2014-03-28 21:02:51 -0400

    remove grunt test, as it merely invokes npm test, but in a complicated way.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var npm = require('npm');
-
 module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-release');
@@ -16,32 +14,11 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.registerTask('test', function() {
-    var done = this.async();
-
-    npm.load({
-      logLevel: 'error'
-    }, function(error) {
-      if(error) {
-        grunt.fail.fatal('Could not load npm');
-        return;
-      }
-
-      npm.runScript('test', function(error) {
-        if(error) {
-          grunt.fail.fatal('tests failed');
-          return;
-        }
-
-        grunt.log.write('tests passed, wtg');
-        done();
-      });
-    });
-  });
-
-  grunt.registerTask('ember:release', ['test', 'release']);
-
-  ['patch', 'major', 'minor'].forEach(function(level) {
+  [
+    'patch',
+    'major',
+    'minor'
+  ].forEach(function(level) {
     grunt.registerTask('ember:release:' + level, ['test', 'release:' + level]);
   });
 };
