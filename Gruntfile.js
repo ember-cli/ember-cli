14b7fec2d1e164fc493ac08be66f14aec3762529
Joe Fiorini <joe@joefiorini.com>
2014-04-03 13:46:43 -0400

    Put grunt test task back until we can get grunt-mocha setup
    This reverts commit acb088c14963598b6472ec50992f7177194e8320.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var npm = require('npm');
+
 module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-release');
@@ -14,14 +16,36 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.registerTask('test', function() {
+    var done = this.async();
+
+    npm.load({
+      logLevel: 'error'
+    }, function(error) {
+      if(error) {
+        grunt.fail.fatal('Could not load npm');
+        return;
+      }
+
+      npm.runScript('test', function(error) {
+        if(error) {
+          grunt.fail.fatal('tests failed');
+          return;
+        }
+
+        grunt.log.write('tests passed, wtg');
+        done();
+      });
+    });
+  });
+
+  grunt.registerTask('ember:release', ['test', 'release']);
+
   [
     'patch',
     'major',
     'minor'
   ].forEach(function(level) {
-    grunt.registerTask('ember:release:' + level, [
-      'test',
-      'release:' + level
-    ]);
+    grunt.registerTask('ember:release:' + level, ['test', 'release:' + level]);
   });
 };
