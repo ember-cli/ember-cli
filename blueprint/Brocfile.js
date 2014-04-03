a90ab4d985beddfd1201114460a298f11a475a86
Joe Fiorini <joe@joefiorini.com>
2014-04-03 12:44:14 -0400

    Always hand tree into CSS preprocessing

--- blueprint/Brocfile.js
+++ blueprint/Brocfile.js
@@ -85,7 +85,7 @@ module.exports = function (broccoli) {
 
   // Styles
 
-  var styles = preprocessCss(sourceTrees, prefix + '/styles', '/assets');
+  var styles = preprocessCss(appAndDependencies, prefix + '/styles', '/assets');
 
   // Ouput
 
