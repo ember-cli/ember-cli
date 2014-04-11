8f86d00c4b1b6a33385786c8ffc562c240e3daef
Mattia Gheda <ghedamat@gmail.com>
2014-04-11 17:13:23 -0400

    use load-initializer vendor'd package
    see https://github.com/stefanpenner/ember-app-kit/pull/588

--- blueprint/app/app.js
+++ blueprint/app/app.js
@@ -1,4 +1,5 @@
 import Resolver from 'ember/resolver';
+import loadInitializers from 'ember/load-initializers';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
@@ -11,5 +12,7 @@ var App = Ember.Application.extend({
   modulePrefix: '<%= modulePrefix %>', // TODO: loaded via config
   Resolver: Resolver
 });
+
+loadInitializers(App, '<%= modulePrefix %>');
 
 export default App;
