e2c9615f6f6cf176f51e34d5712eee7425c567b5
Josef Brandl <mail@josefbrandl.de>
2014-04-13 16:21:26 +0200

    Improve help command test

--- tests/unit/commands/help-test.js
+++ tests/unit/commands/help-test.js
@@ -14,26 +14,31 @@ describe('help command', function() {
     command = rewire('../../../lib/commands/help');
     environment = {
       commands: {
-        testCommand: new Command({
-          name: 'test-command',
+        testCommand1: new Command({
+          name: 'test-command-1',
           description: 'command-description',
           availableOptions: [
             { name: 'option-with-default', type: String, default: 'default-value' },
             { name: 'required-option', type: String, required: 'true', description: 'option-descriptionnnn' }
           ],
           run: function() {}
+        }),
+        testCommand2: new Command({
+          name: 'test-command-2',
+          run: function() {}
         })
       }
     };
   });
 
-  it('should generate good help output', function() {
+  it('should generate complete help output', function() {
     command.run(ui, environment);
-    expect(ui.output[0]).to.include('ember test-command');
+    expect(ui.output[0]).to.include('ember test-command-1');
     expect(ui.output[0]).to.include('command-description');
     expect(ui.output[0]).to.include('option-with-default');
-    expect(ui.output[0]).to.include('default-value');
+    expect(ui.output[0]).to.include('(Default: default-value)');
     expect(ui.output[0]).to.include('required-option');
-    expect(ui.output[0]).to.include('option-description');
+    expect(ui.output[0]).to.include('(Required)');
+    expect(ui.output[0]).to.include('ember test-command-2');
   });
 });
