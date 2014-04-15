bb9842ec4e129402f482624c679db7b18e4b1c8b
Robert Jackson <robert.w.jackson@me.com>
2014-04-15 13:23:04 -0400

    Use config/environments.js to control options to provide to the application.

--- blueprint/app/app.js
+++ blueprint/app/app.js
@@ -4,11 +4,6 @@ import loadInitializers from 'ember/load-initializers';
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
-  LOG_ACTIVE_GENERATION: true,
-  LOG_MODULE_RESOLVER: true,
-  // LOG_TRANSITIONS: true,
-  // LOG_TRANSITIONS_INTERNAL: true,
-  LOG_VIEW_LOOKUPS: true,
   modulePrefix: '<%= modulePrefix %>', // TODO: loaded via config
   Resolver: Resolver
 });
