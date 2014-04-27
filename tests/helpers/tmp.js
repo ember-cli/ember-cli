619856f7c56293d6b4f474150b9455d177eeccdf
Gordon Potter <eccegordo@gmail.com>
2014-04-27 04:48:52 -0700

    align require statements per style guide
    Was reading through source and figured I would line up require statements. Did initial pass through all files and commit as one PR.

--- tests/helpers/tmp.js
+++ tests/helpers/tmp.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var fs = require('fs-extra');
+var fs        = require('fs-extra');
 var mkdirSync = fs.mkdirSync;
-var rimraf = require('rimraf');
-var root = process.cwd();
+var rimraf    = require('rimraf');
+var root      = process.cwd();
 
 module.exports.setup = function(path) {
   process.chdir(root);
