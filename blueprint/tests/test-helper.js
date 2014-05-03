04c959598f972730a8c7125d6431be4ef038bd17
Adolfo Builes <builes.adolfo@gmail.com>
2014-05-03 12:53:27 -0300

    Remove global test helpers.

--- blueprint/tests/test-helper.js
+++ blueprint/tests/test-helper.js
@@ -4,25 +4,3 @@ import { setResolver } from 'ember-qunit';
 setResolver(resolver);
 
 document.write('<div id="ember-testing-container"><div id="ember-testing"></div></div>');
-
-function exists(selector) {
-  return !!window.find(selector).length;
-}
-
-function getAssertionMessage(actual, expected, message) {
-  return message || QUnit.jsDump.parse(expected) + " expected but was " + QUnit.jsDump.parse(actual);
-}
-
-function equal(actual, expected, message) {
-  message = getAssertionMessage(actual, expected, message);
-  QUnit.equal.call(this, actual, expected, message);
-}
-
-function strictEqual(actual, expected, message) {
-  message = getAssertionMessage(actual, expected, message);
-  QUnit.strictEqual.call(this, actual, expected, message);
-}
-
-window.exists = exists;
-window.equal = equal;
-window.strictEqual = strictEqual;
