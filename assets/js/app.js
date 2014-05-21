713fc09c399eec7610a2dc5be8f68e2fcbcc176b
Jon Harmon <jon@jonharmondesign.com>
2014-05-21 09:19:24 -0400

    work in progress

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,4 +8,38 @@
   });
 
   FastClick.attach(document.body);
+
+
+(function() {
+ 
+$(window).scroll(function () {
+  if ( $(this).scrollTop() > $(this).height() ) {
+    $('#sidenav').show();
+   } else if ( $(this).scrollTop() <= $(this).height() ) {
+    $('#sidenav').hide();
+  }
+});
+
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1500);
+        return false;
+      }
+    }
+  });
+});
+})();
+
+var sheight = $('#showcase').css('height');
+
+$('#swap').height(sheight);
+$('.guide-content').css('min-height',sheight);
+
 })();
