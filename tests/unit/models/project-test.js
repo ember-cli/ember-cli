5c498c1e23afbf2d55c2b28572b5e7d6b3613a20
Alex Navasardyan <twokul@users.noreply.github.com>
2014-06-05 16:26:58 -0400

    Ensure that config/environment is findable and required.
    Fixes #907

---
+++ tests/unit/models/project-test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var Project = require('../../../lib/models/project');
+var tmp     = require('../../helpers/tmp');
+var touch   = require('../../helpers/file-utils').touch;
+var assert  = require('assert');
+
+describe('models/project.js', function() {
+  var project;
+  var path = process.cwd() + '/tmp/test-app';
+  var called = false;
+
+  before(function() {
+    Project.prototype.require = function() {
+      called = true;
+      return function() {};
+    };
+
+    tmp.setup(path);
+
+    touch(path + '/config/environment.js', {
+      baseURL: '/foo/bar'
+    });
+
+    project = new Project(path, { });
+  });
+
+  after(function() {
+    tmp.teardown(path);
+  });
+
+  it('config() finds and requires config/environment', function() {
+    project.config('development');
+    assert.equal(called, true);
+  });
+});
