88186ed65d1e87a286e0f6a3f8f5fb5222c26b1d
Kim Røen <kim@pam.as>
2014-06-21 00:21:56 +0200

    Improvements to generator behavior

--- blueprints/serializer/files/tests/unit/serializers/__name__-test.js
+++ blueprints/serializer/files/tests/unit/serializers/__name__-test.js
@@ -2,11 +2,11 @@ import { test, moduleFor } from 'ember-qunit';
 
 moduleFor('serializer:<%= dasherizedModuleName %>', '<%= classifiedModuleName %>Serializer', {
   // Specify the other units that are required for this test.
-  // needs: ['adapter:foo']
+  // needs: ['serializer:foo']
 });
 
 // Replace this with your real tests.
 test('it exists', function() {
-  var adapter = this.subject();
-  ok(adapter);
+  var serializer = this.subject();
+  ok(serializer);
 });
