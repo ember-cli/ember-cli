bfbf6cfda999ba7465298cab859750e321e3c3b3
Jonathan Jackson <jonathan.jackson1@me.com>
2014-06-29 22:16:46 -0400

    [Bugfix] Ensure error on invalid component entityName

--- blueprints/component/index.js
+++ blueprints/component/index.js
@@ -1,4 +1,20 @@
 var Blueprint = require('../../lib/models/blueprint');
 
 module.exports = Blueprint.extend({
+  normalizeEntityName: function(entityName) {
+    Blueprint.prototype.normalizeEntityName.apply(this, arguments);
+
+    if(! /\-/.test(entityName)) {
+      throw new Error('You specified "' + entityName + '", but in order to prevent ' +
+                      'clashes with current or future HTML element names you must have ' +
+                      'a hyphen.\n');
+    }
+
+    if(/\//.test(entityName)) {
+      throw new Error('You specified "' + entityName + '", but due to a bug in ' +
+                      'Handlebars (< 2.0) slashes within components/helpers are not ' +
+                      'allowed.\n');
+    }
+
+  }
 });
