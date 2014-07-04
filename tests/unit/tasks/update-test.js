55f2afedf8b88a93cb94d53fe9945c53dd73aad2
Stefan Penner <stefan.penner@gmail.com>
2014-07-04 12:58:35 -0400

    [fixes #1265] don’t diverge the promise chains (we must wait for the update to occur)
    tests + refactoring

--- tests/unit/tasks/update-test.js
+++ tests/unit/tasks/update-test.js
@@ -10,29 +10,114 @@ describe('update task', function() {
   var updateTask;
   var ui;
 
+  var loadCalledWith;
+  var installCalledWith;
+  var initCommandWasRun;
+
+  var npm = {
+    load: function(options, callback) {
+      setTimeout(function(){
+        callback(undefined, npm);
+      }, 0);
+      loadCalledWith = options;
+    },
+    commands: {
+      install: function(packages, callback) {
+        setTimeout(callback, 0);
+        installCalledWith = packages;
+      }
+    }
+  };
+
   beforeEach(function() {
-    ui = new MockUI();
-    ui.prompt = function(messageObject) {
-      return new Promise(function(resolve) {
-        ui.write(messageObject.message);
-        resolve({
-          answer: false
+    installCalledWith = loadCalledWith = initCommandWasRun = undefined;
+  });
+
+  describe('don\'t update', function() {
+    beforeEach(function() {
+      ui = new MockUI();
+
+      ui.prompt = function(messageObject) {
+        return new Promise(function(resolve) {
+          ui.write(messageObject.message);
+          resolve({
+            answer: false
+          });
         });
+      };
+      updateTask = new UpdateTask({
+        ui: ui,
+        npm: npm
       });
-    };
-    updateTask = new UpdateTask({
-      ui: ui,
+    });
+
+    it('says \'a new version is available\' and asks you to confirm you want to update', function() {
+      return updateTask.run({
+        environment: 'development'
+      }, {
+        newestVersion: '100.0.0'
+      }).then(function() {
+        assert.include(ui.output, 'A new version of ember-cli is available');
+        assert.include(ui.output, 'Are you sure you want to update ember-cli?');
+      });
     });
   });
 
-  it('says "a new version is available" and asks you to confirm you want to update', function() {
-    return updateTask.run({
-      environment: 'development'
-    }, {
-      newestVersion: '100.0.0'
-    }).then(function() {
-      assert.include(ui.output, 'A new version of ember-cli is available');
-      assert.include(ui.output, 'Are you sure you want to update ember-cli?');
+  describe("do update", function() {
+    beforeEach(function() {
+      ui = new MockUI();
+
+      ui.pleasantProgress = {
+        start: function() { },
+        stop: function() { }
+      };
+
+      ui.prompt = function(messageObject) {
+        return new Promise(function(resolve) {
+          ui.write(messageObject.message)
+
+          resolve({
+            answer: true
+          });
+        });
+      };
+
+      function Init() {
+
+      }
+
+      Init.prototype.run = function() {
+        initCommandWasRun = true;
+      }
+      updateTask = new UpdateTask({
+        commands: {
+          Init: Init
+        },
+        ui: ui,
+        npm: npm,
+        project: {
+          root: 'tests/fixtures/dummy-project-outdated',
+          pkg: require('../../fixtures/dummy-project-outdated/package.json')
+        }
+      });
+    });
+
+    it('says \'a new version is available\' and asks you to confirm you want to update', function() {
+      this.timeout(1000000);
+      return updateTask.run({
+        environment: 'development'
+      }, {
+        newestVersion: '100.0.0'
+      }).then(function() {
+        assert.include(ui.output, 'A new version of ember-cli is available');
+        assert.include(ui.output, 'Are you sure you want to update ember-cli?');
+        assert.deepEqual(installCalledWith, [ 'ember-cli' ], '');
+        assert.deepEqual(loadCalledWith, {
+          'global': true,
+          'loglevel': 'silent'
+        }, '');
+        assert(initCommandWasRun);
+      });
     });
   });
 });
