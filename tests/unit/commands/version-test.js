08a43e70b57ad542d843bf59021112fda4b2add6
Ken Browning <ken.browning@hautelook.com>
2014-07-10 14:54:50 -0700

    Add test for version command.

---
+++ tests/unit/commands/version-test.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var assert         = require('../../helpers/assert');
+var commandOptions = require('../../factories/command-options');
+var VersionCommand = require('../../../lib/commands/version');
+var MockUI         = require('../../helpers/mock-ui');
+
+describe('version command', function() {
+  var ui, command, options;
+
+  beforeEach(function() {
+    ui = new MockUI();
+    options = commandOptions({
+      ui: ui,
+      project: {
+        isEmberCLIProject: function() {
+          return false;
+        }
+      }
+    });
+
+    command = new VersionCommand(options);
+  });
+
+  it('reports node and npm versions', function() {
+    command.validateAndRun();
+    var lines = ui.output.split('\n');
+    assert(someLineStartsWith(lines, 'node:'), 'contains the version of node');
+    assert(someLineStartsWith(lines, 'npm:'), 'contains the version of npm');
+  });
+
+  it('supports a --verbose flag', function() {
+    command.validateAndRun(['--verbose']);
+    var lines = ui.output.split('\n');
+    assert(someLineStartsWith(lines, 'node:'), 'contains the version of node');
+    assert(someLineStartsWith(lines, 'npm:'), 'contains the version of npm');
+    assert(someLineStartsWith(lines, 'v8:'), 'contains the version of v8');
+  });
+
+});
+
+function someLineStartsWith(lines, search) {
+  return lines.some(function(line) {
+    return line.indexOf(search) === 0;
+  });
+}
