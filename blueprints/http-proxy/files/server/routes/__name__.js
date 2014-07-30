1488e7dc6984efae4d5aeba85426897310d30aae
Trek Glowacki <trek@groupon.com>
2014-07-30 14:01:02 -0500

    Add http-proxy generator/blueprint

---
+++ blueprints/http-proxy/files/server/routes/__name__.js
@@ -0,0 +1,8 @@
+module.exports = function(app) {
+  var express = require('express');
+  var <%= camelizedModuleName %>Router = express.Router();
+  <%= camelizedModuleName %>Router.get('/', function(req, res) {
+    res.send({<%= dasherizedModuleName %>:[]});
+  });
+  app.use('/api<%= path %>', <%= camelizedModuleName %>Router);
+};
