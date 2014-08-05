b89f0a5748559d642d39ea771fc047770f615a4e
Trek Glowacki <trek@groupon.com>
2014-08-05 13:59:20 -0500

    Make proxies with with requests that have bodies.

--- blueprints/http-proxy/files/server/index.js
+++ blueprints/http-proxy/files/server/index.js
@@ -9,7 +9,8 @@
 
 var bodyParser = require('body-parser');
 var globSync   = require('glob').sync;
-var routes     = globSync('./*/**/*.js', { cwd: __dirname }).map(require);
+var mocks      = globSync('./mocks/**/*.js', { cwd: __dirname }).map(require);
+var proxies    = globSync('./proxies/**/*.js', { cwd: __dirname }).map(require);
 
 module.exports = function(app) {
   app.use(bodyParser.json());
@@ -17,5 +18,12 @@ module.exports = function(app) {
     extended: true
   }));
 
-  routes.forEach(function(route) { route(app); });
+  mocks.forEach(function(route) { route(app)});
+
+  // proxy expects a stream, but express will have turned
+  // the request stream into an object because bodyParser
+  // has run. We have to convert it back to stream:
+  // https://github.com/nodejitsu/node-http-proxy/issues/180
+  app.use(require('connect-restreamer')());
+  proxies.forEach(function(route) { route(app)});
 };
