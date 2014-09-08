60fbbdeee7d437999195f13ea88e96c81a0f8d32
Jay Phelps <hello@jayphelps.com>
2014-09-07 21:08:20 -0700

    Moved modulePrefix to config/environment.js

--- blueprints/app/files/app/app.js
+++ blueprints/app/files/app/app.js
@@ -1,14 +1,15 @@
 import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
+import config from './config/environment';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
-  modulePrefix: '<%= modulePrefix %>', // TODO: loaded via config
+  modulePrefix: config.modulePrefix,
   Resolver: Resolver
 });
 
-loadInitializers(App, '<%= modulePrefix %>');
+loadInitializers(App, config.modulePrefix);
 
 export default App;
--- blueprints/app/files/config/environment.js
+++ blueprints/app/files/config/environment.js
@@ -2,6 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
+    modulePrefix: '<%= modulePrefix %>',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
