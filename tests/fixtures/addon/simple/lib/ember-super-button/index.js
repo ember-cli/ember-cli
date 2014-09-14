4a73bcda85e06ad58498f6918ae29a0ddd3a475b
Robert Jackson <robert.w.jackson@me.com>
2014-09-14 07:59:47 -0400

    Refactor addon fixtures.
    Migrate to currently suggested exporting of an object.

--- tests/fixtures/addon/simple/lib/ember-super-button/index.js
+++ tests/fixtures/addon/simple/lib/ember-super-button/index.js
@@ -1,27 +1,5 @@
 'use strict';
 
-var path = require('path');
-var fs   = require('fs');
-
-function EmberSuperButton(project) {
-  this.project = project;
-  this.name = 'Ember Super Button';
-}
-
-function unwatchedTree(dir){
-  return {read: function() { return dir; }};
-}
-
-EmberSuperButton.prototype.treeFor = function treeFor(name) {
-  var treePath = path.join(__dirname, name);
-
-  if (fs.existsSync(treePath)) {
-    return unwatchedTree(treePath);
-  }
+module.exports = {
+  name: 'Ember Super Button',
 };
-
-EmberSuperButton.prototype.included = function included(app) {
-  this.app = app;
-};
-
-module.exports = EmberSuperButton;
