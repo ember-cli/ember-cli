a2188f6592b347303c544ee6b502019f775fd88f
Brendan Graetz <brendan.graetz@gmail.com>
2014-09-23 12:46:00 +1000

    =BG= minor - avoid if statement to make code clearer.
    updated for comment: https://github.com/stefanpenner/ember-cli/pull/2082/files#r17889770

--- blueprints/app/files/tests/test-helper.js
+++ blueprints/app/files/tests/test-helper.js
@@ -8,5 +8,5 @@ setResolver(resolver);
 document.write('<div id="ember-testing-container"><div id="ember-testing"></div></div>');
 
 QUnit.config.urlConfig.push({ id: 'nocontainer', label: 'Hide container'});
-document.getElementById('ember-testing-container').style.visibility =
-  (QUnit.config.urlParams.nocontainer ? 'hidden' : 'visible');
+var containerVisibility = QUnit.config.urlParams.nocontainer ? 'hidden' : 'visible';
+document.getElementById('ember-testing-container').style.visibility = containerVisibility;
