f00df082d5e1a26d0fbf6206864586abcfd5da6e
Robert Jackson <robert.w.jackson@me.com>
2014-09-23 07:19:24 -0400

    Move config into meta.

--- blueprints/app/files/tests/helpers/start-app.js
+++ blueprints/app/files/tests/helpers/start-app.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 import Application from '../../app';
 import Router from '../../router';
-import config from '../../config/environments/test';
+import config from '../../config/environment';
 
 export default function startApp(attrs) {
   var App;
--- tests/fixtures/brocfile-tests/custom-ember-env/config/environment.js
+++ tests/fixtures/brocfile-tests/custom-ember-env/config/environment.js
@@ -1,5 +1,6 @@
 module.exports = function() {
   return  {
+    modulePrefix: 'some-cool-app',
     EmberENV: {
       asdflkmawejf: ';jlnu3yr23'
     }
