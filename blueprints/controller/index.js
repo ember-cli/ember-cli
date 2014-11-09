4070bfa0e524df00fcea6ca7fc7cafb5f3858ac2
Robert Jackson <robert.w.jackson@me.com>
2014-11-09 12:26:57 -0500

    Remove controller types.
    In preparation for the coming deprecation in Ember of
    `Ember.ObjectController` and `Ember.ArrayController`. This will make
    future upgrades easier (users will not need to refactor away from
    proxying behavior).

--- blueprints/controller/index.js
+++ blueprints/controller/index.js
@@ -1,36 +1,3 @@
-var SilentError = require('../../lib/errors/silent');
-var chalk       = require('chalk');
-
-var TYPE_MAP = {
-  array: 'ArrayController',
-  object: 'ObjectController',
-  basic: 'Controller'
-};
-
 module.exports = {
-  description: 'Generates a controller of the given type.',
-
-  availableOptions: [
-    { name: 'type', values: ['basic', 'object', 'array'], default: 'basic' }
-  ],
-
-  beforeInstall: function(options) {
-    var type = options.type;
-
-    if (type && !TYPE_MAP[type]) {
-      throw new SilentError('Unknown controller type "' + type + '". Should be "basic", "object", or "array".');
-    }
-
-    if (!type && !options.installingTest) {
-      this.ui.writeLine(chalk.yellow('Warning: no controller type was specified, defaulting to basic. Specify using --type=basic|object|array'));
-    }
-  },
-
-  locals: function(options) {
-    var baseClass = TYPE_MAP[options.type] || TYPE_MAP.basic;
-
-    return {
-      baseClass: baseClass
-    };
-  }
+  description: 'Generates a controller of the given type.'
 };
