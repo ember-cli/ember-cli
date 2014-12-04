62d2f4f683b4e18310428ce5a74e6e49dcbad15a
toran billups <toranb@gmail.com>
2014-12-03 21:53:33 -0600

    The generated meta tag is now self closing

--- tests/unit/broccoli/ember-app-test.js
+++ tests/unit/broccoli/ember-app-test.js
@@ -125,7 +125,7 @@ describe('broccoli/ember-app', function() {
       it('includes the `meta` tag in `head` by default', function() {
         var escapedConfig = escape(JSON.stringify(config));
         var metaExpected = '<meta name="cool-foo/config/environment" ' +
-                           'content="' + escapedConfig + '">';
+                           'content="' + escapedConfig + '" />';
         var actual = emberApp.contentFor(config, defaultMatch, 'head');
 
         assert(actual.indexOf(metaExpected) > -1);
@@ -136,7 +136,7 @@ describe('broccoli/ember-app', function() {
 
         var escapedConfig = escape(JSON.stringify(config));
         var metaExpected = '<meta name="cool-foo/config/environment" ' +
-                           'content="' + escapedConfig + '">';
+                           'content="' + escapedConfig + '" />';
         var actual = emberApp.contentFor(config, defaultMatch, 'head');
 
         assert(actual.indexOf(metaExpected) === -1);
