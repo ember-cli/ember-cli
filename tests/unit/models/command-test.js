4bc04e913dbb345d869e5ebf6b104a2d647d9720
Jake Bixby <trabus@gmail.com>
2014-12-06 13:03:43 -0800

    Refactor command model and added command unit tests

--- tests/unit/models/command-test.js
+++ tests/unit/models/command-test.js
@@ -325,7 +325,8 @@ describe('models/command.js', function() {
     var register = optionsAlias.registerOptions.bind(optionsAlias);
 
     optionsAlias.availableOptions = extendedAvailableOptions;
-    expect(register).to.throw('The "carne-asada" alias is already in use by the "--filling" option and cannot be used by the "--favorite" option. Please use a different alias.');
+    expect(register).to.throw('The "carne-asada" alias is already in use by the "--filling" option and ' +
+      'cannot be used by the "--favorite" option. Please use a different alias.');
 
   });
 
@@ -363,38 +364,90 @@ describe('models/command.js', function() {
     var register = optionsAlias.registerOptions.bind(optionsAlias);
 
     optionsAlias.availableOptions = badArrayAvailableOptions;
-    expect(register).to.throw('The "[object Object]" [type:array] alias is not an acceptable value. It must be a string or single key object with a string value (for example, "value" or { "key" : "value" }).');
+    expect(register).to.throw('The "[object Object]" [type:array] alias is not an acceptable value. ' +
+      'It must be a string or single key object with a string value (for example, "value" or { "key" : "value" }).');
 
     optionsAlias.availableOptions = badObjectAvailableOptions;
-    expect(register).to.throw('The "[object Object]" [type:object] alias is not an acceptable value. It must be a string or single key object with a string value (for example, "value" or { "key" : "value" }).');
+    expect(register).to.throw('The "[object Object]" [type:object] alias is not an acceptable value. ' +
+      'It must be a string or single key object with a string value (for example, "value" or { "key" : "value" }).');
 
   });
 
-  it('parseAlias() should parse aliases', function() {
-    var option = { name: 'filling', type: String, default: 'adobada', aliases:[
-          {'carne-asada' : 'carne-asada'}, {'carnitas' : 'carnitas' }, {'fish' : 'fish'}
-          ]
-        };
-    var alias = {'carnitas' : 'carnitas' };
-    expect(new OptionsAliasCommand({
+  it('parseAlias() should parse aliases and return an object', function() {
+    var optionsAlias = new OptionsAliasCommand({
       ui: ui,
       analytics: analytics,
       project: project,
       settings: {}
-    }).parseAlias(option, alias)).to.be.ok;
+    });
+    var option = { name: 'filling', type: String, key: 'filling', default: 'adobada', aliases:[
+        {'carne-asada' : 'carne-asada'}, {'carnitas' : 'carnitas' }, {'fish' : 'fish'}
+      ]
+    };
+    var alias = {'carnitas' : 'carnitas' };
+    expect(optionsAlias.parseAlias(option, alias)).to.deep.equal({
+      key: 'carnitas',
+      value: ['--filling','carnitas'],
+      original: {'carnitas' : 'carnitas'}
+    });
   });
 
   it('validateOption() should validate options', function() {
     var option = { name: 'filling', type: String, default: 'adobada', aliases:[
-          {'carne-asada' : 'carne-asada'}, {'carnitas' : 'carnitas' }, {'fish' : 'fish'}
-          ]
-        };
-    expect(new OptionsAliasCommand({
-      ui: ui,
+        {'carne-asada' : 'carne-asada'}, {'carnitas' : 'carnitas' }, {'fish' : 'fish'}
+      ]
+    };
+    var dupe = { name: 'spicy', type: Boolean, default: true, aliases:[{'mild' : false}] };
+    var noAlias = { name: 'reload', type: Boolean, default: false};
+    expect(new OptionsAliasCommand({ui: ui,
       analytics: analytics,
       project: project,
       settings: {}
     }).validateOption(option)).to.be.ok;
+
+    expect(new ServeCommand({ui: ui,
+      analytics: analytics,
+      project: project,
+      settings: {}
+    }).validateOption(noAlias)).to.be.false;
+
+    expect(new OptionsAliasCommand({ui: ui,
+      analytics: analytics,
+      project: project,
+      settings: {}
+    }).validateOption(dupe)).to.be.false;
+  });
+
+  it('validateOption() should throw an error when option is missing name or type', function() {
+    var optionsAlias = new OptionsAliasCommand({
+      ui: ui,
+      analytics: analytics,
+      project: project,
+      settings: {}
+    });
+    var notype = { name: 'taco'};
+    var noname = { type: Boolean };
+
+    expect(optionsAlias.validateOption.bind(optionsAlias, notype)).to.throw('The command "options-alias" has an ' +
+      'option without the required type and name fields.');
+    expect(optionsAlias.validateOption.bind(optionsAlias, noname)).to.throw('The command "options-alias" has an ' +
+      'option without the required type and name fields.');
+  });
+
+  it('validateOption() should throw an error when option name is camelCase or capitalized', function() {
+    var optionsAlias = new OptionsAliasCommand({
+      ui: ui,
+      analytics: analytics,
+      project: project,
+      settings: {}
+    });
+    var capital = { name: 'Taco', type: Boolean};
+    var camel = { name: 'tacoTown', type: Boolean };
+
+    expect(optionsAlias.validateOption.bind(optionsAlias, capital)).to.throw('The "Taco" option\'s name of the "options-alias"' +
+      ' command contains a capital letter.');
+    expect(optionsAlias.validateOption.bind(optionsAlias, camel)).to.throw('The "tacoTown" option\'s name of the "options-alias"' +
+      ' command contains a capital letter.');
   });
 
   it('mergeDuplicateOption() should merge duplicate options together', function() {
@@ -404,6 +457,10 @@ describe('models/command.js', function() {
       project: project,
       settings: {}
     });
+    var garbageAvailableOptions = [
+        { name: 'spicy', type: Boolean, default: true, aliases:[{'mild' : true}] }
+      ];
+    optionsAlias.registerOptions( { availableOptions : garbageAvailableOptions });
     var extendedAvailableOptions = [{ name: 'filling', type: String, default: 'adobada', aliases:[
         {'carne-asada' : 'carne-asada'}, {'carnitas' : 'carnitas' }, {'fish' : 'fish'}
         ]
@@ -414,9 +471,8 @@ describe('models/command.js', function() {
       }];
     optionsAlias.registerOptions( { availableOptions : extendedAvailableOptions });
     optionsAlias.availableOptions.push(duplicateExtendedAvailableOptions[0]);
-    optionsAlias.mergeDuplicateOption( 'filling' );
 
-    expect(optionsAlias.availableOptions).to.deep.equal([
+    expect(optionsAlias.mergeDuplicateOption( 'filling' )).to.deep.equal([
       { name: 'taco',
         type: String,
         default: 'traditional',
