99191a68a0fafa9997889535f5d3683a9cc53a48
Jake Craige <jake@poeticsystems.com>
2014-12-19 08:38:06 -0600

    Add podModulePrefix to app blueprint
    This provides a nice default for people transitioning to the pods structure as
    well as makes this option known.

--- blueprints/app/files/config/environment.js
+++ blueprints/app/files/config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: '<%= modulePrefix %>',
+    podModulePrefix: '<%= modulePrefix %>/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
