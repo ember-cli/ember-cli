225e53a1f6de6d23f3db5e4efb738e63621d7146
Jake Craige <jake@poeticsystems.com>
2014-12-19 08:36:56 -0600

    Revert "Add podModulePrefix to app blueprint"
    This reverts commit f6ada104a403e48ae0aa64174a7e5ef1baca0e72.

--- blueprints/app/files/config/environment.js
+++ blueprints/app/files/config/environment.js
@@ -3,7 +3,6 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: '<%= modulePrefix %>',
-    podModulePrefix: '<%= modulePrefix %>/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
