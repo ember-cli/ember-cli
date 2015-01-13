2be5ac4a1dc2f5e879c09814733e594dccf65273
Gabor Babicz <gabor.babicz@gmail.com>
2015-01-13 13:36:12 +0100

    +space in test-helper app blueprint

--- blueprints/app/files/tests/test-helper.js
+++ blueprints/app/files/tests/test-helper.js
@@ -7,6 +7,6 @@ setResolver(resolver);
 
 document.write('<div id="ember-testing-container"><div id="ember-testing"></div></div>');
 
-QUnit.config.urlConfig.push({ id: 'nocontainer', label: 'Hide container'});
+QUnit.config.urlConfig.push({ id: 'nocontainer', label: 'Hide container' });
 var containerVisibility = QUnit.urlParams.nocontainer ? 'hidden' : 'visible';
 document.getElementById('ember-testing-container').style.visibility = containerVisibility;
