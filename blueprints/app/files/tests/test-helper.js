4482f0e08a5093d17ea1b2140977177565406ca1
Edward Faulkner <ef@alum.mit.edu>
2015-01-25 12:14:20 -0500

    switch to test-framework-agnostic test loader

--- blueprints/app/files/tests/test-helper.js
+++ blueprints/app/files/tests/test-helper.js
@@ -1,3 +1,4 @@
+/* global require */
 import resolver from './helpers/resolver';
 import {
   setResolver
@@ -10,3 +11,22 @@ document.write('<div id="ember-testing-container"><div id="ember-testing"></div>
 QUnit.config.urlConfig.push({ id: 'nocontainer', label: 'Hide container' });
 var containerVisibility = QUnit.urlParams.nocontainer ? 'hidden' : 'visible';
 document.getElementById('ember-testing-container').style.visibility = containerVisibility;
+
+QUnit.config.urlConfig.push({ id: 'nojshint', label: 'Disable JSHint'});
+
+if (QUnit.notifications) {
+  QUnit.notifications({
+    icons: {
+      passed: '/assets/passed.png',
+      failed: '/assets/failed.png'
+    }
+  });
+}
+
+$(document).ready(function(){
+  var TestLoader = require('ember-cli/test-loader')['default'];
+  TestLoader.prototype.shouldLoadModule = function(moduleName) {
+    return moduleName.match(/[-_]test$/) || (!QUnit.urlParams.nojshint && moduleName.match(/\.jshint$/));
+  };
+  TestLoader.load();
+});
