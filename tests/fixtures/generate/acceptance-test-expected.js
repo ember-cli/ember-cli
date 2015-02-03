07055c25b67fb3d13c071185ff55ac00823473e3
Kori Roys <kori@koriroys.com>
2015-02-03 11:43:13 +0100

    allow generated nested acceptance tests
    An example of what currently doesn't work:
    
        ember g acceptance-test projects/create
    
    (because '../helpers/start-app' doesn't exist)
    
    Opening this to start a discussion. Probably you guys know a better solution.

--- tests/fixtures/generate/acceptance-test-expected.js
+++ tests/fixtures/generate/acceptance-test-expected.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 import { module, test } from 'qunit';
-import startApp from '../helpers/start-app';
+import startApp from 'my-app/tests/helpers/start-app';
 
 var application;
 
