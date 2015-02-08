cd84f6d89232e5505e22ae49cdbf104f87ff294f
Chad Hietala <chadhietala@gmail.com>
2015-02-08 13:13:20 -0800

    Use defaultLocationType for location resolution

--- blueprints/app/files/config/environment.js
+++ blueprints/app/files/config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: '<%= modulePrefix %>',
     environment: environment,
     baseURL: '/',
-    locationType: 'auto',
+    defaultLocationType: 'auto',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
