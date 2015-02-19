c24514683113945208cae8ac077e5ac63fe1445b
Robert Jackson <robert.w.jackson@me.com>
2015-02-19 15:17:49 -0500

    Remove express & glob from default app package.json.
    These packages are only needed when used with certain blueprints, which
    can add what they need/want.

--- blueprints/server/index.js
+++ blueprints/server/index.js
@@ -5,7 +5,8 @@ module.exports = {
 
   afterInstall: function() {
     return this.addPackagesToProject([
-      { name: 'morgan', target: '^1.3.2' }
+      { name: 'morgan', target: '^1.3.2' },
+      { name: 'glob', target: '^4.0.5' }
     ]);
   }
 };
