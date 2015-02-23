b95c9adec3e35ba1a9f03dd39e86fb103ea91268
Robert Jackson <robert.w.jackson@me.com>
2015-02-22 21:35:36 -0500

    Ensure that an addons styles tree is in the same path as the apps.

--- tests/unit/models/addon-test.js
+++ tests/unit/models/addon-test.js
@@ -12,6 +12,9 @@ var path    = require('path');
 var findWhere = require('lodash/collection/find');
 var MockUI = require('../../helpers/mock-ui');
 
+var broccoli  = require('broccoli');
+var walkSync  = require('walk-sync');
+
 var root    = process.cwd();
 var tmproot = path.join(root, 'tmp');
 
@@ -553,6 +556,42 @@ describe('models/addon.js', function() {
 
     it('is provided with the addon\'s `ui` object', function() {
       expect(discovery.ui).to.equal(ui);
+    });
+  });
+
+  describe('treeForStyles', function() {
+    var builder, addon;
+
+    beforeEach(function() {
+      var BaseAddon = Addon.extend({
+        name: 'base-addon'
+      });
+      addon = new BaseAddon(project);
+    });
+
+    afterEach(function() {
+      if (builder) {
+        return builder.cleanup();
+      }
+    });
+
+    it('should move files in the root of the addons app/styles tree into the app/styles path', function() {
+      addon.root = path.join(fixturePath, 'with-app-styles');
+
+      builder = new broccoli.Builder(addon.treeFor('styles'));
+
+      return builder.build()
+        .then(function(results) {
+          var outputPath = results.directory;
+
+          var expected = [
+            'app/',
+            'app/styles/',
+            'app/styles/foo-bar.css'
+          ];
+
+          expect(walkSync(outputPath)).to.eql(expected);
+        });
     });
   });
 });
