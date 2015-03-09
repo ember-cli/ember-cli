44b7d553a6f39fbda729aa9ff884b42d31d630d0
Simon Prévost <sprevost@mirego.com>
2015-03-09 10:04:35 -0400

    Update component-test test.js blueprint
    The component-test blueprint is the only test template to have the word "specify" in lowercase.

--- blueprints/component-test/files/tests/unit/__path__/__test__.js
+++ blueprints/component-test/files/tests/unit/__path__/__test__.js
@@ -4,18 +4,18 @@ import {
 } from 'ember-qunit';
 
 moduleForComponent('<%= dasherizedModuleName %>', {
-  // specify the other units that are required for this test
+  // Specify the other units that are required for this test
   // needs: ['component:foo', 'helper:bar']
 });
 
 test('it renders', function(assert) {
   assert.expect(2);
 
-  // creates the component instance
+  // Creates the component instance
   var component = this.subject();
   assert.equal(component._state, 'preRender');
 
-  // renders the component to the page
+  // Renders the component to the page
   this.render();
   assert.equal(component._state, 'inDOM');
 });
