907c875f4ede8d44a6a7dcc3c53971e92b102533
Ilya Radchenko <ilya@burstcreations.com>
2015-03-26 13:41:35 -0400

    Add rootUrl to router

--- blueprints/app/files/app/router.js
+++ blueprints/app/files/app/router.js
@@ -2,7 +2,8 @@ import Ember from 'ember';
 import config from './config/environment';
 
 var Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
+  rootURL: config.baseURL
 });
 
 Router.map(function() {
