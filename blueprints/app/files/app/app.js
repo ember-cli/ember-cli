2adafa7cc874dc48dabc27565bce070deda0415c
Ilya Radchenko <ilya@burstcreations.com>
2015-04-16 10:05:10 -0400

    Remove podModulePrefix from app.js
    I think this was left over after removing `podModulePrefix` from the config. If not, just close this PR.

--- blueprints/app/files/app/app.js
+++ blueprints/app/files/app/app.js
@@ -9,7 +9,6 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
-  podModulePrefix: config.podModulePrefix,
   Resolver: Resolver
 });
 
