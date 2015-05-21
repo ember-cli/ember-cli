35dca15f44edcbcbfd9edb387fa96da1fdd3bd35
Jake Bixby <trabus@users.noreply.github.com>
2015-05-21 10:01:14 -0700

    Revert "Remove podModulePrefix from app.js"

--- blueprints/app/files/app/app.js
+++ blueprints/app/files/app/app.js
@@ -9,6 +9,7 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
+  podModulePrefix: config.podModulePrefix,
   Resolver: Resolver
 });
 
