9a50397989d7e0f19f94ed91a51a030b66ad0bec
Cat Bieber <catbieber@users.noreply.github.com>
2015-05-21 21:40:29 -0400

    Close #3751 Check parents of the root directory for outputPath

--- tests/unit/models/builder-test.js
+++ tests/unit/models/builder-test.js
@@ -72,6 +72,7 @@ describe('models/builder.js', function() {
 
   describe('Prevent deletion of files for improper outputPath', function() {
     var command;
+    var parentPath = '..' + path.sep + '..' + path.sep;
 
     before(function() {
       command = new BuildCommand(commandOptions({
@@ -86,15 +87,13 @@ describe('models/builder.js', function() {
       });
     });
 
-    it('when outputPath is root directory ie., `--output-path=/`', function() {
-      var outputPathArg = '--output-path=/';
+    it('when outputPath is root directory ie., `--output-path=/` or `--output-path=C:`', function() {
+      var outputPathArg = '--output-path=.';
       var outputPath = command.parseArgs([outputPathArg]).options.outputPath;
+      outputPath = outputPath.split(path.sep)[0] + path.sep;
       builder.outputPath = outputPath;
 
-      return builder.clearOutputPath()
-        .catch(function(error) {
-          expect(error.message).to.equal('Using a build destination path of `' + outputPath + '` is not supported.');
-        });
+      expect(builder.canDeleteOutputPath(outputPath)).to.equal(false);
     });
 
     it('when outputPath is project root ie., `--output-path=.`', function() {
@@ -102,21 +101,24 @@ describe('models/builder.js', function() {
       var outputPath = command.parseArgs([outputPathArg]).options.outputPath;
       builder.outputPath = outputPath;
 
-      return builder.clearOutputPath()
-        .catch(function(error) {
-          expect(error.message).to.equal('Using a build destination path of `' + outputPath + '` is not supported.');
-        });
+      expect(builder.canDeleteOutputPath(outputPath)).to.equal(false);
     });
 
-    it('when outputPath is a parent directory ie., `--output-path=../../`', function() {
-      var outputPathArg = '--output-path=../../';
+    it('when outputPath is a parent directory ie., `--output-path=' + parentPath + '`', function() {
+      var outputPathArg = '--output-path=' + parentPath;
       var outputPath = command.parseArgs([outputPathArg]).options.outputPath;
       builder.outputPath = outputPath;
 
-      return builder.clearOutputPath()
-        .catch(function(error) {
-          expect(error.message).to.equal('Using a build destination path of `' + outputPath + '` is not supported.');
-        });
+      expect(builder.canDeleteOutputPath(outputPath)).to.equal(false);
+    });
+
+    it('allow outputPath to contain the root path as a substring, as long as it is not a parent', function() {
+      var outputPathArg = '--output-path=.';
+      var outputPath = command.parseArgs([outputPathArg]).options.outputPath;
+      outputPath = outputPath.substr(0, outputPath.length - 1);
+      builder.outputPath = outputPath;
+
+      expect(builder.canDeleteOutputPath(outputPath)).to.equal(true);
     });
   });
 
