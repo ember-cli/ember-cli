99ea0ec5de9c8c234cb705e2fc06f59f6bb0ef46
Stefan Penner <stefan.penner@gmail.com>
2015-07-17 08:34:13 -0700

    [fixes #4328] patch engion.io-client to use any XMLHTTPRequest, but then bundle the one it wanted in ember-cli’s tarball
    pending: https://github.com/socketio/engine.io-client/issues/405

---
+++ patches/patch-engine-io-client.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+/*
+ * patch engion.io-client to use any XMLHTTPRequest, but then bundle the one it
+ * wanted in ember-cli’s tarball
+ *
+ * pending: https://github.com/socketio/engine.io-client/issues/405
+ */
+
+var fs = require('fs')
+var packagePath = __dirname + '/../node_modules/testem/node_modules/socket.io/node_modules/socket.io-client/node_modules/engine.io-client/package.json';
+var package = JSON.parse(fs.readFileSync(packagePath, 'UTF-8'));
+
+if (package.dependencies.xmlhttprequest !== 'https://github.com/rase-/node-XMLHttpRequest/archive/a6b6f2.tar.gz') {
+  throw new Error('engine.io-clients deps changed, XMLHTTPRequest may be fixed');
+}
+package.dependencies.xmlhttprequest === '*';
+
+fs.writeFileSync(packagePath, JSON.stringify(packagePath));
