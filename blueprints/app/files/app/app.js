10eaa28c3d51cf01a24bd14c93272a9d8f9c039c
Maurice Mohlek <mohlek@gmail.com>
2015-08-03 10:56:16 +0200

    Update app.js
    Fix warning
    Property assignment should use enhanced object literal function. `{ propName: propName}` is not allowed.

--- blueprints/app/files/app/app.js
+++ blueprints/app/files/app/app.js
@@ -10,7 +10,7 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver: Resolver
+  Resolver
 });
 
 loadInitializers(App, config.modulePrefix);
