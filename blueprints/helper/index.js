161b9e4d165019f8b9af899b8af0d25e720db9dd
lazybensch <lazybensch@gmail.com>
2015-08-14 06:57:30 +0200

    remove non-dash helper warning

--- blueprints/helper/index.js
+++ blueprints/helper/index.js
@@ -1,6 +1,5 @@
 'use strict';
 /*jshint node:true*/
-var chalk = require('chalk');
 var Blueprint   = require('../../lib/models/blueprint');
 
 module.exports = {
@@ -8,9 +7,6 @@ module.exports = {
   normalizeEntityName: function(entityName) {
     entityName = Blueprint.prototype.normalizeEntityName.apply(this, arguments);
 
-    if (entityName.indexOf('-') === -1) {
-      this.ui.write(chalk.yellow('[WARN]: helpers without a dash will not automatically be resolved \n'));
-    }
     return entityName;
   }
 };
