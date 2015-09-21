cc65668f9065d300e0daee49a3a201bfcca9c663
Xavier Cambar <xcambar@gmail.com>
2015-09-22 00:28:38 +0200

    Updates blueprints to follow ember-suave

--- blueprints/app/files/app/app.js
+++ blueprints/app/files/app/app.js
@@ -3,14 +3,12 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
 
-var App;
-
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
-App = Ember.Application.extend({
+const App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
-  Resolver: Resolver
+  Resolver
 });
 
 loadInitializers(App, config.modulePrefix);
