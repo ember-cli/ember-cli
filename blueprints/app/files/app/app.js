65197368f3ec5b346444d93dff5d6acb27c0ddba
Xavier Cambar <xcambar@gmail.com>
2015-09-22 16:23:08 +0200

    variable declaration at the top; missing space

--- blueprints/app/files/app/app.js
+++ blueprints/app/files/app/app.js
@@ -3,9 +3,11 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
 
+let App;
+
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
-const App = Ember.Application.extend({
+App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver
