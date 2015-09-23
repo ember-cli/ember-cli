dd761814bda166205516028e358716fad957a2ed
Xavier Cambar <xcambar@gmail.com>
2015-09-24 01:23:03 +0200

    Remove dead code

--- tests/helpers/acceptance.js
+++ tests/helpers/acceptance.js
@@ -142,13 +142,6 @@ function teardownTestTargets() {
   });
 }
 
-function cleanupDependencies() {
-  var npmDir    = path.resolve(path.join(root, '.node_modules-tmp'));
-  var bowerDir  = path.resolve(path.join(root, '.bower_components-tmp'));
-  fs.removeSync(npmDir);
-  fs.removeSync(bowerDir);
-}
-
 /**
  * Creates symbolic links from the dependency temp directories
  * to the project that is under test.
@@ -202,6 +195,5 @@ module.exports = {
   createTestTargets: createTestTargets,
   linkDependencies: linkDependencies,
   teardownTestTargets: teardownTestTargets,
-  cleanupRun: cleanupRun,
-  cleanupDependencies: cleanupDependencies
+  cleanupRun: cleanupRun
 };
