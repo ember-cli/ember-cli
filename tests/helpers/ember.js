c28df67a878a588857b710b769a0db65aeeb2def
Jake Bixby <trabus@gmail.com>
2015-10-09 12:45:59 -0700

    added fixture package.json for ember helper

--- tests/helpers/ember.js
+++ tests/helpers/ember.js
@@ -3,6 +3,7 @@
 var MockUI        = require('./mock-ui');
 var MockAnalytics = require('./mock-analytics');
 var Cli           = require('../../lib/cli');
+var path          = require('path');
 
 module.exports = function ember(args) {
   var cli;
@@ -15,7 +16,13 @@ module.exports = function ember(args) {
     cliArgs:      args,
     Leek: MockAnalytics,
     UI: MockUI,
-    testing: true
+    testing: true,
+    cli: {
+      // This prevents ember-cli from detecting any other package.json files
+      // forcing ember-cli to act as the globally installed package
+      npmPackage: 'ember-cli',
+      root: path.resolve(__dirname, '..', 'fixtures', 'package')
+    }
   });
 
   return cli;
