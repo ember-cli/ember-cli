410fb50ee81e784f39d04eb97e2e1b19bf90a746
Ilya Radchenko <ilya@burstcreations.com>
2015-10-26 20:39:57 -0400

    Use shorthand methods for ED transform blueprint
    Promotes users to use ES6 shorthand methods by making them the default.

--- blueprints/transform/files/__root__/__path__/__name__.js
+++ blueprints/transform/files/__root__/__path__/__name__.js
@@ -1,11 +1,11 @@
 import DS from 'ember-data';
 
 export default DS.Transform.extend({
-  deserialize: function(serialized) {
+  deserialize(serialized) {
     return serialized;
   },
 
-  serialize: function(deserialized) {
+  serialize(deserialized) {
     return deserialized;
   }
 });
