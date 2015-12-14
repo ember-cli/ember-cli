fb75145709b6bcb31c29e82c13ac412337850efb
Robert Jackson <robert.w.jackson@me.com>
2015-12-14 11:36:36 -0500

    Fix instance-initializer-test blueprint.
    * Ensure context is correct when assigning `this.application` and `this.buildInstance`
    * Fix invalid reference to `application` (should be `this.application`)
    * Import `destroyApp` helper (was being referenced without an import)

--- blueprints/instance-initializer-test/files/tests/unit/instance-initializers/__name__-test.js
+++ blueprints/instance-initializer-test/files/tests/unit/instance-initializers/__name__-test.js
@@ -1,12 +1,13 @@
 import Ember from 'ember';
 import { initialize } from '<%= dependencyDepth %>/instance-initializers/<%= dasherizedModuleName %>';
 import { module, test } from 'qunit';
+import destroyApp from '../../helpers/destroy-app';
 
 module('<%= friendlyTestName %>', {
   beforeEach: function() {
-    Ember.run(function() {
+    Ember.run(() => {
       this.application = Ember.Application.create();
-      this.appInstance = application.buildInstance();
+      this.appInstance = this.application.buildInstance();
     });
   },
   afterEach: function() {
