42291a9b73a3b292d66527d4021d2990ea291e65
Martin Broerse <broerse@martinic.com>
2015-12-27 07:51:08 +0100

    Fix typo

--- blueprints/addon/files/ember-cli-build.js
+++ blueprints/addon/files/ember-cli-build.js
@@ -8,7 +8,7 @@ module.exports = function(defaults) {
   });
 
   /*
-    This build file specifes the options for the dummy test app of this
+    This build file specifies the options for the dummy test app of this
     addon, located in `/tests/dummy`
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
