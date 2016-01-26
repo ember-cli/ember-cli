cc2bc3d722ea715d1f47b58f24fb8205705ca7c6
Stefan Penner <stefan.penner@gmail.com>
2016-01-26 00:57:10 -0800

    fixup formatting

--- tests/unit/utilities/find-build-file-test.js
+++ tests/unit/utilities/find-build-file-test.js
@@ -8,36 +8,36 @@ var findBuildFile = require('../../../lib/utilities/find-build-file');
 describe('find-build-file', function() {
   var tmpPath, tmpFilename, tmpFilePath;
   var currentWorkingDir = process.cwd();
-	
+
   beforeEach(function() {
     tmpPath = process.cwd()+'/tmp/';
-	tmpFilename = 'ember-cli-build.js';
-	tmpFilePath = tmpPath + tmpFilename;
+    tmpFilename = 'ember-cli-build.js';
+    tmpFilePath = tmpPath + tmpFilename;
     return tmp.setup(tmpPath)
-	  .then(function(){
-	    process.chdir(tmpPath);
+      .then(function(){
+        process.chdir(tmpPath);
       });
   });
-  
+
   afterEach(function() {
-	  delete require.cache[require.resolve(tmpFilePath)];
-	  return tmp.teardown(tmpPath).then(function(){
-	  	process.chdir(currentWorkingDir);
-	  });
+    delete require.cache[require.resolve(tmpFilePath)];
+    return tmp.teardown(tmpPath).then(function(){
+      process.chdir(currentWorkingDir);
+    });
   });
-  
+
   it('does not throws an error when the file is valid syntax', function() {
-  	fs.writeFileSync(tmpFilename, 'module.exports = function(){return {\'a\': \'A\', \'b\': \'B\'};}', { encoding: 'utf8' });
-	
-	expect(function() {
+    fs.writeFileSync(tmpFilename, 'module.exports = function(){return {\'a\': \'A\', \'b\': \'B\'};}', { encoding: 'utf8' });
+
+    expect(function() {
       findBuildFile(tmpFilename);
     }).to.not.throw();
   });
 
-  it('throws an SyntaxError if the file contains a syntax mistake', function(){  
-	fs.writeFileSync(tmpFilename, 'module.exports = function(){return {\'a\': \'A\' \'b\': \'B\'};}', { encoding: 'utf8' });
-	
-    expect(function() {  
+  it('throws an SyntaxError if the file contains a syntax mistake', function(){
+    fs.writeFileSync(tmpFilename, 'module.exports = function(){return {\'a\': \'A\' \'b\': \'B\'};}', { encoding: 'utf8' });
+
+    expect(function() {
       findBuildFile(tmpFilename);
     }).to.throw(SyntaxError, /Could not require '.*':/);
   });
