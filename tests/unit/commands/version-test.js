d0e1a17b6c017c088de58d63e46ba2c9e4c8d53b
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-03-21 21:49:29 +0100

    tests/commands/version: Check if "ember-cli" is printed

--- tests/unit/commands/version-test.js
+++ tests/unit/commands/version-test.js
@@ -23,6 +23,7 @@ describe('version command', function() {
   it('reports node, npm, and os versions', function() {
     return command.validateAndRun().then(function() {
       var lines = options.ui.output.split(EOL);
+      expect(someLineStartsWith(lines, 'ember-cli:'), 'contains the version of ember-cli').to.be.ok;
       expect(someLineStartsWith(lines, 'node:'), 'contains the version of node').to.be.ok;
       expect(someLineStartsWith(lines, 'os:'), 'contains the version of os').to.be.ok;
     });
