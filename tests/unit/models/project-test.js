c5ba6db738ef83ab4cd9374653fa717f31f10518
Marten Schilstra <mail@martenschilstra.nl>
2016-04-07 18:36:24 +0200

    Close #5751 - Unable to name a add-on/project as a substring of "ember-core"

--- tests/unit/models/project-test.js
+++ tests/unit/models/project-test.js
@@ -461,6 +461,16 @@ describe('models/project.js', function() {
       var addon = project.findAddonByName('not-an-addon');
       expect(addon).to.equal(undefined, 'not found addon should be undefined');
     });
+
+    it('should not return an addon that is a substring of requested name', function() {
+      var addon = project.findAddonByName('foo-ba');
+      expect(addon).to.equal(undefined, 'foo-ba should not be found');
+    })
+
+    it('should return an addon that is the unqualified name of requested name', function() {
+      var addon = project.findAddonByName('qux/foo');
+      expect(addon.name).to.equal('foo', 'should have found the foo addon');
+    })
   });
 
   describe('bowerDirectory', function() {
