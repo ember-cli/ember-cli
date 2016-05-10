085070932a5b9acc86dacffa176c0ad786ed551b
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-05-10 22:31:36 +0200

    blueprints/blueprint: Create .jshintrc only if "ember-cli-jshint" is a dep

--- blueprints/blueprint/index.js
+++ blueprints/blueprint/index.js
@@ -1,3 +1,21 @@
 module.exports = {
-  description: 'Generates a blueprint and definition.'
+  description: 'Generates a blueprint and definition.',
+
+  files: function() {
+    var files = this._super.files.apply(this, arguments);
+
+    if (!this.hasJSHint()) {
+      files = files.filter(function(file) {
+        return file !== 'blueprints/.jshintrc';
+      });
+    }
+
+    return files;
+  },
+
+  hasJSHint: function() {
+    if (this.project) {
+      return 'ember-cli-jshint' in this.project.dependencies();
+    }
+  }
 };
