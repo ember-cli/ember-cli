6f3d5d31d089d4c9f49288e664ac2c439c046dc4
Nathan Hammond <github.com@nathanhammond.com>
2016-06-10 23:45:31 -0700

    Revert "Split the test into two."
    This reverts commit 6d3d2d56d31de0a39eb2b9e2c6572f274b2d6a62.

--- tests/acceptance/addon-smoke-test-slow.js
+++ tests/acceptance/addon-smoke-test-slow.js
@@ -108,6 +108,16 @@ describe('Acceptance: addon-smoke-test', function() {
       });
   });
 
+  it('can add modules via `{{content-for "head"}}`', function() {
+    return copyFixtureFiles('addon/content-for-head')
+      .then(function() {
+        return ember(['test']);
+      })
+      .then(function(result) {
+        expect(result.exitCode).to.eql(0);
+      });
+  });
+
   it('build with only pod templates', function() {
     return copyFixtureFiles('addon/pod-templates-only')
       .then(function() {
