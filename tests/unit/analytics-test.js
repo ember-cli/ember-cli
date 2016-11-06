cc0f8c1b2bf045068cd944d587b79e38b51e7401
Alex Navasardyan <anavasardyan@linkedin.com>
2016-11-06 15:31:03 -0800

    [BUGFIX] Makes sure that only internal commands are tracked
    Before this PR we were tracking all of commands (ember-cli + custom).
    We probably want to be smarter about it and think of a better way to
    expose tracking info. For now, we likely should limit tracking only
    for internal commands.

--- tests/unit/analytics-test.js
+++ tests/unit/analytics-test.js
@@ -4,39 +4,63 @@ var expect = require('chai').expect;
 var Command = require('../../lib/models/command');
 var MockUI = require('../helpers/mock-ui');
 var MockProject = require('../helpers/mock-project');
-var command;
+var Promise = require('../../lib/ext/promise');
+var analyticsUtils = require('../../lib/utilities/analytics-utils');
+
+var ANALYTICS_WHITELISTED_COMMANDS = analyticsUtils.ANALYTICS_WHITELISTED_COMMANDS;
+
+var command, serveCommand, TmpCommand;
 var called = false;
 
-beforeEach(function() {
-  var analytics = {
-    track: function() {
-      called = true;
-    }
-  };
+var analytics = {
+  track: function() {
+    called = true;
+  }
+};
+var project = new MockProject();
+project.isEmberCLIProject = function() { return true; };
 
-  var FakeCommand = Command.extend({
-    name: 'fake-command',
-    run: function() {}
-  });
-
-  var project = new MockProject();
-  project.isEmberCLIProject = function() { return true; };
-
-  command = new FakeCommand({
+// populate an array of commands that we expect to be tracked
+var commands = [];
+for (var i = 0, l = ANALYTICS_WHITELISTED_COMMANDS.length; i < l; i++) {
+  TmpCommand = Command.extend({
+    name: ANALYTICS_WHITELISTED_COMMANDS[i],
+    run: function(options) { return options; }
+  })
+  commands.push(new TmpCommand({
     ui: new MockUI(),
     analytics: analytics,
     project: project
-  });
-});
-
-afterEach(function() {
-  command = null;
-});
+  }));
+}
 
 describe('analytics', function() {
-  it('track gets invoked on command.validateAndRun()', function() {
+  afterEach(function() {
+    called = false;
+  });
+
+  it('track gets invoked on command.validateAndRun() only for whitelisted commands', function() {
+    return Promise.all(commands.map(function(command) {
+      return command.validateAndRun([]).then(function() {
+        expect(called, command + ': expected analytics.track to be called').to.be.true;
+      });
+    }));
+  });
+
+  it('track does not get invoked on command.validateAndRun() for non-whitelisted commands', function() {
+    var CustomCommand = Command.extend({
+      name: 'custom-command',
+      run: function() {}
+    });
+
+    command = new CustomCommand({
+      ui: new MockUI(),
+      analytics: analytics,
+      project: project
+    });
+
     return command.validateAndRun([]).then(function() {
-      expect(called, 'expected analytics.track to be called').to.be.true;
+      expect(called, 'expected analytics.track not to be called').to.be.false;
     });
   });
 });
