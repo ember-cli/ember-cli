75693802dadb6b1086b4271a4b043a6cde7e80c4
Nathan Hammond <github.com@nathanhammond.com>
2016-11-21 15:55:51 -0800

    Clean up fixture helpers.

---
+++ tests/acceptance/css-test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var AppFixture = require('../helpers/app-fixture');
+var AddonFixture = require('../helpers/addon-fixture');
+
+function generateStyleAddon(name) {
+  var addonFixture = new AddonFixture(name);
+  addonFixture._generateCSS('app/styles/addon.css');
+  addonFixture._generateCSS('app/styles/app.css');
+  addonFixture._generateCSS('app/styles/_import.css');
+  addonFixture._generateCSS('app/styles/' + addonFixture.name + '.css');
+  addonFixture._generateCSS('app/styles/alpha.css');
+  addonFixture._generateCSS('app/styles/zeta.css');
+  addonFixture._generateCSS('addon/styles/addon.css');
+  addonFixture._generateCSS('addon/styles/app.css');
+  addonFixture._generateCSS('addon/styles/_import.css');
+  addonFixture._generateCSS('addon/styles/' + addonFixture.name + '.css');
+  addonFixture._generateCSS('addon/styles/alpha.css');
+  addonFixture._generateCSS('addon/styles/zeta.css');
+
+  return addonFixture;
+}
+
+function generateRootApplication(name) {
+  return new AppFixture(name);
+}
+
+var root = generateRootApplication('root');
+var foo = generateStyleAddon('foo');
+root.install('npm', foo);
+// root.serialize();
+
+console.log(JSON.stringify(root, undefined, 2));
