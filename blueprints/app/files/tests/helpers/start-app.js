4b8a23a5cbdfe4d24fa35d69694519112ec9ff66
Mark David Avery <thewebark@gmail.com>
2016-11-30 16:25:24 -0800

    Revert "Revert "Revert "Changed the start-app test helper to use `Ember.assign` instead of `Ember.merge`."""
    This reverts commit 7b282ae9afd46aef9943c765db8aa97a6efc3f83.

--- blueprints/app/files/tests/helpers/start-app.js
+++ blueprints/app/files/tests/helpers/start-app.js
@@ -5,8 +5,8 @@ import config from '../../config/environment';
 export default function startApp(attrs) {
   let application;
 
-  // use defaults, but you can override
-  let attributes = Ember.assign({}, config.APP, attrs);
+  let attributes = Ember.merge({}, config.APP);
+  attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
 
   Ember.run(() => {
     application = Application.create(attributes);
