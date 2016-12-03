0f9690db0e27e210a7e3848c9fd0eb003f7f0eaa
Stefan Penner <stefan.penner@gmail.com>
2016-12-03 15:50:32 -0800

    WIP: fix missing git scenarios

--- tests/unit/tasks/git-init-test.js
+++ tests/unit/tasks/git-init-test.js
@@ -11,17 +11,21 @@ var path        = require('path');
 var root        = process.cwd();
 var mkTmpDirIn  = require('../../../lib/utilities/mk-tmp-dir-in');
 var tmproot     = path.join(root, 'tmp');
+var td          = require('testdouble');
 
 describe('git-init', function() {
-  var subject, ui, tmpdir;
+  var subject, ui, tmpdir, exec;
 
   beforeEach(function() {
+    exec = td.function('exec');
+
     return mkTmpDirIn(tmproot).then(function(dir) {
       tmpdir  = dir;
       ui      = new MockUI();
       subject = new GitInitTask({
         ui: ui,
-        project: new MockProject()
+        project: new MockProject(),
+        exec: exec
       });
       process.chdir(tmpdir);
     });
@@ -33,20 +37,48 @@ describe('git-init', function() {
     return remove(tmproot);
   });
 
-/**
-* TODO: git commit not working with this test setup, it errors out.
-* We need to be able to
-*/
-  it('skipGit properly skips git-init', function() {
-    return subject.run({skipGit:true}).then(function() {
-      expect(ui.output).to.not.include('Successfully initialized git.');
+  describe('skipGit: true', function() {
+    it('does not initialize git', function() {
+      td.when(exec()).thenResolve();
+
+      return subject.run({
+        skipGit: true
+      }).then(function() {
+        expect(ui.output).to.not.include('Successfully initialized git.');
+        td.verify(exec('git --version'), { times: 0 });
+      });
     });
   });
 
-  it('errors are logged with logErrors', function() {
-    return subject.run({skipGit: false, logErrors: true}).then(function() {
-      expect(ui.output).to.equal('');
-      expect(ui.errors).to.not.equal('');
+  it('correctly initializes git if git is around, and more or less works', function() {
+    td.when(exec(td.matchers.contains('git --version'))).thenResolve();
+    td.when(exec(td.matchers.contains('git init'))).thenResolve();
+    td.when(exec(td.matchers.contains('git add .'))).thenResolve();
+    td.when(exec(td.matchers.contains('git commit -m'))).thenResolve();
+
+    return subject.run().then(function() {
+      td.verify(exec(td.matchers.contains('git --version')));
+      td.verify(exec(td.matchers.contains('git init')));
+      td.verify(exec(td.matchers.contains('git add .')));
+      td.verify(exec(td.matchers.contains('git commit -m "'), td.matchers.anything()));
+
+      expect(ui.output).to.contain('Successfully initialized git.');
+      expect(ui.errors).to.equal('');
+    });
+  });
+
+
+  it('skips initializing git, if `git --version` fails', function() {
+    td.when(exec(td.matchers.contains('git --version'))).thenReject();
+
+    return subject.run().then(function() {
+      td.verify(exec(td.matchers.contains('git --version')), { times: 1 });
+      td.verify(exec(td.matchers.contains('git init')), { times: 0 });
+      td.verify(exec(td.matchers.contains('git add .')), { times: 0 });
+      td.verify(exec(td.matchers.contains('git commit -m "'), td.matchers.anything()), { times: 0 });
+
+      expect(ui.output).to.contain('');
+      expect(ui.errors).to.equal('');
     });
   });
 });
