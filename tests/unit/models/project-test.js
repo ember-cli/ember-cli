bacb837f723916f0992eff4876bf6db73b19c3c2
Robert Jackson <rjackson@linkedin.com>
2016-12-05 23:00:46 -0500

    Use correct configKey for ember-cli-babel specific options.
    Newer versions of ember-cli-babel will begin deprecating passing
    any options in `babel` config hash that are not destined for `babel`
    itself (this includes `compileModules` and `includePolyfill`).
    
    This detects the newer version and uses the appropriate (non-deprecated)
    configuration property.
    
    It also moves `findAddonByName` into a shared utility method (to ensure
    both project and addon use the same mechanism).

--- tests/unit/models/project-test.js
+++ tests/unit/models/project-test.js
@@ -436,35 +436,14 @@ describe('models/project.js', function() {
       td.verify(project.initializeAddons(), {ignoreExtraArgs: true});
     });
 
-    it('should return the foo addon from name', function() {
-      var addon = project.findAddonByName('foo');
+    it('generally should work and defer to findAddonByName utlity', function() {
+      var addon;
+      addon = project.findAddonByName('foo');
       expect(addon.name).to.equal('foo', 'should have found the foo addon');
-    });
 
-    it('should return the foo-bar addon from name when a foo also exists', function() {
-      var addon = project.findAddonByName('foo-bar');
-      expect(addon.name).to.equal('foo-bar', 'should have found the foo-bar addon');
-    });
-
-    it('should return the bar-pkg addon from package name', function() {
-      var addon = project.findAddonByName('bar-pkg');
+      addon = project.findAddonByName('bar-pkg');
       expect(addon.pkg.name).to.equal('bar-pkg', 'should have found the bar-pkg addon');
     });
-
-    it('should return undefined if addon doesn\'t exist', function() {
-      var addon = project.findAddonByName('not-an-addon');
-      expect(addon).to.equal(undefined, 'not found addon should be undefined');
-    });
-
-    it('should not return an addon that is a substring of requested name', function() {
-      var addon = project.findAddonByName('foo-ba');
-      expect(addon).to.equal(undefined, 'foo-ba should not be found');
-    });
-
-    it('should not guess addon name from string with slashes', function() {
-      var addon = project.findAddonByName('qux/foo');
-      expect(addon).to.equal(undefined, 'should not have found the foo addon');
-    })
   });
 
   describe('bowerDirectory', function() {
