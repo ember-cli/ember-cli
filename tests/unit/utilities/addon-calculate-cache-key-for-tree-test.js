eb81d16773359fb581706d693c57a887c72b00fc
Robert Jackson <rjackson@linkedin.com>
2016-12-15 21:37:32 -0800

    Add basic tests covering.

---
+++ tests/unit/utilities/addon-calculate-cache-key-for-tree-test.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var calculateCacheKeyForTree = require('../../../lib/utilities/addon-calculate-cache-key-for-tree');
+var chai = require('../../chai');
+var expect = chai.expect;
+
+describe('calculateCacheKeyForTree', function() {
+  it('should return same value for same input', function() {
+    var addon = {
+      root: 'hoy',
+      pkg: { some: 'parsed', value: { goes: 'here' }}
+    };
+
+    var first = calculateCacheKeyForTree('app', addon);
+    var second = calculateCacheKeyForTree('app', addon);
+
+    expect(first).to.equal(second);
+  });
+
+  it('should return different value when different', function() {
+    var addon = {
+      root: 'hoy',
+      pkg: { some: 'other', value: { goes: 'elsewhere' }}
+    };
+
+    var first = calculateCacheKeyForTree('app', addon);
+    var second = calculateCacheKeyForTree('app', addon);
+
+    expect(first).to.equal(second);
+  });
+
+  it('should not return same value for same addon with different tree type', function() {
+    var addon = {
+      root: 'hoy',
+      pkg: { some: 'parsed', value: { goes: 'here' }}
+    };
+
+    var first = calculateCacheKeyForTree('addon', addon);
+    var second = calculateCacheKeyForTree('app', addon);
+
+    expect(first).not.to.equal(second);
+  });
+});
