5c777337e2852833ec9af01e2b87e84079030e17
Nathan Hammond <github.com@nathanhammond.com>
2016-12-20 11:24:13 -0800

    Rework fixture helpers to reduce complexity.

--- tests/helpers/addon-fixture.js
+++ tests/helpers/addon-fixture.js
@@ -1,43 +1,42 @@
-var fs = require('fs');
+var fs = require('fs-extra');
 var path = require('path');
-var stringUtil = require('ember-cli-string-utils');
+
+var fixturify = require('fixturify');
+var quickTemp = require('quick-temp');
+
+var originalWorkingDirectory = process.cwd();
+var root = path.resolve(__dirname, '..', '..');
+
+var PackageCache = require('../../tests/helpers/package-cache');
+var CommandGenerator = require('../../tests/helpers/command-generator');
+
+var packageCache = new PackageCache(root);
+var ember = new CommandGenerator(path.join(root, 'bin', 'ember'));
+
+var flags = [
+  '--disable-analytics',
+  '--watcher=node',
+  '--skip-npm',
+  '--skip-bower',
+  '--skip-git'
+];
 
 var AppFixture = require('./app-fixture');
-
-var blueprintShim = require('./blueprint-shim');
 
 function AddonFixture(name, options) {
   this.type = 'addon';
   this.name = name;
-  this.options = options || {
-    useGlobalPackages: true
-  };
 
-  this.blueprintPath = path.resolve(__dirname, '../../blueprints/addons');
+  process.chdir(root);
+  this.dir = quickTemp.makeOrRemake({}, this.name + '-addon-fixture');
+  process.chdir(originalWorkingDirectory);
 
-  this.fixture = {};
-  this._fixtureCache = {};
-  this.dirs = {};
-
-  this._generatePackageJSON();
-  this._generateBowerJSON();
-
-  var context = {
-    addonModulePrefix: stringUtil.dasherize(this.name),
-    dasherizedModuleName: stringUtil.dasherize(this.name)
-  };
+  ember.invoke('addon', this.name, '--directory=' + this.dir, ...flags);
+  this.fixture = fixturify.readSync(this.dir);
 }
 
 AddonFixture.prototype = Object.create(AppFixture.prototype);
 AddonFixture.prototype.constructor = AddonFixture;
-
-AddonFixture.prototype._generatePackageJSON = function() {
-  this.fixture['package.json'] = blueprintShim['addon']['package.json'];
-};
-
-AddonFixture.prototype._generateBowerJSON = function() {
-  this.fixture['bower.json'] = blueprintShim['addon']['bower.json'];
-};
 
 AddonFixture.prototype.before = function(addon) {
   var config = this.getPackageJSON();
