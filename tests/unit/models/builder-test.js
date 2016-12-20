2809c3412dcde5ede0826b87b8d523470b650dbf
David J. Hamilton <dhamilton@linkedin.com>
2016-12-20 09:32:08 -0800

    wip; instrument at start

--- tests/unit/models/builder-test.js
+++ tests/unit/models/builder-test.js
@@ -160,69 +160,6 @@ describe('models/builder.js', function() {
     });
   }
 
-  describe('._vizEnabled', function() {
-    var originalWarn = console.warn;
-    var warnInvocations;
-
-    beforeEach(function() {
-      delete process.env.BROCCOLI_VIZ;
-      delete process.env.EMBER_CLI_INSTRUMENTATION;
-      warnInvocations = [];
-      console.warn = function () {
-        warnInvocations.push.apply(warnInvocations, Array.prototype.slice.call(arguments));
-      };
-    });
-
-    it('is true and does not warn if BROCCOLI_VIZ=1', function() {
-      process.env.BROCCOLI_VIZ = '1';
-      expect(Builder._vizEnabled()).to.eql(true);
-      expect(warnInvocations).to.eql([]);
-    });
-
-    it('is true and warns at most once if BROCCOLI_VIZ is set but not 1', function() {
-      process.env.BROCCOLI_VIZ = 'on';
-      expect(Builder._vizEnabled()).to.eql(true);
-      expect(Builder._vizEnabled()).to.eql(true);
-      expect(warnInvocations).to.eql([
-        "Please set BROCCOLI_VIZ=1 to enable visual instrumentation, rather than 'on'"
-      ]);
-    });
-
-    it('is false if BROCCOLI_VIZ is unset', function() {
-      expect('BROCCOLI_VIZ' in process.env).to.eql(false);
-      expect(Builder._vizEnabled()).to.eql(false);
-      expect(warnInvocations).to.eql([]);
-    });
-  });
-
-  describe('._instrumentationEnabled', function() {
-    beforeEach(function() {
-      delete process.env.BROCCOLI_VIZ;
-      delete process.env.EMBER_CLI_INSTRUMENTATION;
-    });
-
-    it('is true if BROCCOLI_VIZ=1', function() {
-      process.env.BROCCOLI_VIZ = '1';
-      expect(Builder._instrumentationEnabled()).to.eql(true);
-    });
-
-    it('is true if EMBER_CLI_INSTRUMENTATION=1', function() {
-      process.env.EMBER_CLI_INSTRUMENTATION = '1';
-      expect(Builder._instrumentationEnabled()).to.eql(true);
-    });
-
-    it('is false if EMBER_CLI_INSTRUMENTATION != 1', function() {
-      process.env.EMBER_CLI_INSTRUMENTATION = 'on';
-      expect(Builder._instrumentationEnabled()).to.eql(false);
-    });
-
-    it('is false if both BROCCOLI_VIZ and EMBER_CLI_INSTRUMENTATION are unset', function() {
-      expect('BROCCOLI_VIZ' in process.env).to.eql(false);
-      expect('EMBER_CLI_INSTRUMENTATION' in process.env).to.eql(false);
-      expect(Builder._instrumentationEnabled()).to.eql(false);
-    });
-  });
-
   describe('._computeVizInfo', function() {
     function StatsSchema() {
       this.x = 0;
