c8123f8288d2f9ec5b3950018a5e9a322163ee90
Nathan Hammond <github.com@nathanhammond.com>
2016-12-23 22:41:20 -0500

    Add test fixtures.

--- tests/acceptance/nested-preprocessor-test.js
+++ tests/acceptance/nested-preprocessor-test.js
@@ -11,11 +11,11 @@ var setupPreprocessorRegistryFixture = function(type, registry) {
   var addon = this;
 
   registry.add('<%= type %>', {
-    name: '<%= name %>',
+    name: '<%= name %>' + '-' + type,
     ext: '<%= ext %>',
     toTree: function(tree) {
       return stew.map(tree, function(content, relativePath) {
-        return '//<%= name %>\n' + content;
+        return '// <%= name %>' + '-' + type + '\n' + content;
       });
     }
   });
@@ -69,6 +69,52 @@ describe.only('Acceptance: nested preprocessor tests.', function() {
 
     var child = new InRepoAddonFixture('child-addon');
 
+    inRepoAddons['css'].generateCSS('app/styles/app.css');
+    inRepoAddons['css'].generateCSS('app/styles/addon.css');
+    inRepoAddons['css'].generateCSS('app/styles/_import.css');
+    inRepoAddons['css'].generateCSS('app/styles/' + inRepoAddons['css'].name + '.css');
+    inRepoAddons['css'].generateCSS('app/styles/alpha.css');
+    inRepoAddons['css'].generateCSS('app/styles/zeta.css');
+    inRepoAddons['css'].generateCSS('addon/styles/addon.css');
+    inRepoAddons['css'].generateCSS('addon/styles/app.css');
+    inRepoAddons['css'].generateCSS('addon/styles/_import.css');
+    inRepoAddons['css'].generateCSS('addon/styles/' + inRepoAddons['css'].name + '.css');
+    inRepoAddons['css'].generateCSS('addon/styles/alpha.css');
+    inRepoAddons['css'].generateCSS('addon/styles/zeta.css');
+
+    inRepoAddons['js']._npmAddonInstall({ name: 'ember-cli-babel' });
+    inRepoAddons['js'].generateJS('app/routes/hat.js');
+    inRepoAddons['js'].generateJS('app/routes/cat.js');
+    inRepoAddons['js'].generateJS('addon/components/thing-one.js');
+    inRepoAddons['js'].generateJS('addon/components/thing-two.js');
+
+    inRepoAddons['template']._npmAddonInstall({ name: 'ember-cli-htmlbars' });
+    inRepoAddons['template'].generateTemplate('app/templates/hoist.hbs');
+    inRepoAddons['template'].generateTemplate('addon/templates/anchor.hbs');
+
+    nestedInRepoAddons['css'].generateCSS('app/styles/app.css');
+    nestedInRepoAddons['css'].generateCSS('app/styles/addon.css');
+    nestedInRepoAddons['css'].generateCSS('app/styles/_import.css');
+    nestedInRepoAddons['css'].generateCSS('app/styles/' + nestedInRepoAddons['css'].name + '.css');
+    nestedInRepoAddons['css'].generateCSS('app/styles/alpha.css');
+    nestedInRepoAddons['css'].generateCSS('app/styles/zeta.css');
+    nestedInRepoAddons['css'].generateCSS('addon/styles/addon.css');
+    nestedInRepoAddons['css'].generateCSS('addon/styles/app.css');
+    nestedInRepoAddons['css'].generateCSS('addon/styles/_import.css');
+    nestedInRepoAddons['css'].generateCSS('addon/styles/' + nestedInRepoAddons['css'].name + '.css');
+    nestedInRepoAddons['css'].generateCSS('addon/styles/alpha.css');
+    nestedInRepoAddons['css'].generateCSS('addon/styles/zeta.css');
+
+    nestedInRepoAddons['js']._npmAddonInstall({ name: 'ember-cli-babel' });
+    nestedInRepoAddons['js'].generateJS('app/routes/hat.js');
+    nestedInRepoAddons['js'].generateJS('app/routes/cat.js');
+    nestedInRepoAddons['js'].generateJS('addon/components/thing-one.js');
+    nestedInRepoAddons['js'].generateJS('addon/components/thing-two.js');
+
+    nestedInRepoAddons['template']._npmAddonInstall({ name: 'ember-cli-htmlbars' });
+    nestedInRepoAddons['template'].generateTemplate('app/templates/hoist.hbs');
+    nestedInRepoAddons['template'].generateTemplate('addon/templates/anchor.hbs');
+
     root.install(child);
     root.install(inRepoAddons['css']);
     root.install(inRepoAddons['js']);
@@ -78,9 +124,11 @@ describe.only('Acceptance: nested preprocessor tests.', function() {
     child.install(nestedInRepoAddons['template']);
     root.serialize();
   });
+
   after(function() {
-    root.destroy();
+    // root.clean();
   });
+
   beforeEach(function() {});
   afterEach(function() {});
 
