2a9d13840845a1f6097173bdb988b84363579fb5
Robert Jackson <rjackson@linkedin.com>
2016-12-31 12:06:47 -0500

    Move `module.exports` above constructor declaration.

--- tests/helpers/mock-cli.js
+++ tests/helpers/mock-cli.js
@@ -3,11 +3,10 @@
 var MockUI          = require('console-ui/mock');
 var Instrumentation = require('../../lib/models/instrumentation');
 
+module.exports = MockCLI;
 function MockCLI(options) {
   options = options || {};
 
   this.ui = options.ui || new MockUI();
   this.instrumentation = options.instrumentation || new Instrumentation({});
 }
-
-module.exports = MockCLI;
