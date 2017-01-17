81346bc3d4ae73e3ac40a9c9e0f1cc387ac5f33c
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-01-17 10:19:21 +0100

    tasks/bower-install: Add unit tests for ensureBower() method

--- tests/unit/tasks/bower-install-test.js
+++ tests/unit/tasks/bower-install-test.js
@@ -2,31 +2,60 @@
 
 const BowerInstallTask = require('../../../lib/tasks/bower-install');
 const MockUI = require('console-ui/mock');
-const MockProject = require('../../helpers/mock-project');
-const expect = require('chai').expect;
+const Promise = require('rsvp').Promise;
+const expect = require('../../chai').expect;
+const td = require('testdouble');
 
-describe('bower install task', function() {
-  let bowerInstallTask;
-  let ui;
+describe('BowerInstallTask', function() {
+  let task, ui;
 
   beforeEach(function() {
-    let project = new MockProject();
     ui = new MockUI();
+    task = new BowerInstallTask({ ui });
+  });
 
-    bowerInstallTask = new BowerInstallTask({
-      ui,
-      project,
+  describe('ensureBower()', function() {
+    beforeEach(function() {
+      task.resolveBower = td.function();
+      task.importBower = td.function();
+      task.installBower = td.function();
     });
-  });
 
-  afterEach(function() {
-    ui = undefined;
-    bowerInstallTask = undefined;
-  });
+    it('resolves if "bower" property is set', function() {
+      let mockValue = 'foobar';
+      task.bower = mockValue;
 
-  it('skips bower installs if there is no bower.json', function() {
-    return bowerInstallTask.run({}).then(() => {
-      expect(ui.output).to.include('Skipping bower install: bower.json not found');
+      td.when(task.resolveBower()).thenReject(new Error());
+
+      return task.ensureBower().then(() => {
+        expect(task.bower).to.equal(mockValue);
+      });
+    });
+
+    it('imports "bower" if it can be resolved', function() {
+      td.when(task.resolveBower()).thenResolve('path/to/bower');
+      td.when(task.importBower('path/to/bower')).thenReturn('ok');
+
+      return task.ensureBower().then(ok => {
+        expect(ok).to.equal('ok');
+      });
+    });
+
+    it('install "bower" if it can not be resolved', function() {
+      td.when(task.resolveBower()).thenReturn(Promise.reject(), Promise.resolve('path/to/bower'));
+      td.when(task.installBower()).thenResolve();
+      td.when(task.importBower('path/to/bower')).thenReturn('ok');
+
+      return task.ensureBower().then(ok => {
+        expect(ok).to.equal('ok');
+      });
+    });
+
+    it('pass install errors on', function() {
+      td.when(task.resolveBower()).thenReturn(Promise.reject());
+      td.when(task.installBower()).thenReject(new Error('foobar'));
+
+      return expect(task.ensureBower()).to.be.rejectedWith('foobar');
     });
   });
 });
