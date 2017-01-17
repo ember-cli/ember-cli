82516b7c2e84f364b2a3fea4e5db44b10f257a2f
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-01-17 09:26:38 +0100

    tests/acceptance/generate: Fix flaky tests
    We have to wait for the first outputFile() too before we can continue

--- tests/acceptance/generate-test.js
+++ tests/acceptance/generate-test.js
@@ -348,10 +348,12 @@ describe('Acceptance: ember generate', function() {
   it('passes custom cli arguments to blueprint options', function() {
     return initApp()
       .then(function() {
-        outputFile(
+        return outputFile(
           'blueprints/customblue/files/app/__name__.js',
           "Q: Can I has custom command? A: <%= hasCustomCommand %>"
         );
+      })
+      .then(function() {
         return outputFile(
           'blueprints/customblue/index.js',
           "module.exports = {\n" +
