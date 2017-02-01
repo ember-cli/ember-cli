e502b671cc4a219473c8e812a247e3741f771b34
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-02-01 20:58:10 +0100

    Revert "Auto merge of #6731 - rwjblue:revert-bower-maybe, r=rwjblue"
    This reverts commit cb6bac632dc8dc1c49b30583f0fa135364c5c408, reversing
    changes made to be142aaf7801bf64f4322583c7d82ae7c7066c52.

--- blueprints/addon/index.js
+++ blueprints/addon/index.js
@@ -60,14 +60,6 @@ module.exports = {
     this._writeContentsToFile(sortPackageJson(contents), 'package.json');
   },
 
-  generateBowerJson() {
-    let contents = this._readContentsFromFile('bower.json');
-
-    contents.name = '<%= addonName %>';
-
-    this._writeContentsToFile(contents, 'bower.json');
-  },
-
   afterInstall() {
     let packagePath = path.join(this.path, 'files', 'package.json');
     let bowerPath = path.join(this.path, 'files', 'bower.json');
@@ -107,7 +99,6 @@ module.exports = {
     let appFiles = this._appBlueprint.files();
 
     this.generatePackageJson();
-    this.generateBowerJson();
 
     let addonFiles = walkSync(path.join(this.path, 'files'));
 
