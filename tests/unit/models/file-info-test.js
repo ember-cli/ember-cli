2a8daf9ae3611ece93cfe9a5e06900e8667ad975
kon <kon@539.ch>
2017-02-11 04:14:49 +0100

    New Test: Unit - FileInfo - normalizes line endings before comparing files, fix replace on promise

--- tests/unit/models/file-info-test.js
+++ tests/unit/models/file-info-test.js
@@ -168,4 +168,14 @@ describe('Unit - FileInfo', function() {
     });
   });
 
+  it('normalizes line endings before comparing files', function() {
+    validOptions.inputPath = path.resolve(__dirname, '../../fixtures/file-info/test_crlf.js');
+    validOptions.outputPath = path.resolve(__dirname, '../../fixtures/file-info/test_lf.js');
+    let fileInfo = new FileInfo(validOptions);
+
+    return fileInfo.checkForConflict().then(function(type) {
+      expect(type).to.equal('identical');
+    });
+  });
+
 });
