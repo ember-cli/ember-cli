da1e6ece08a015e94cd0fdf5c921b53ee25d7e4f
Stefan Penner <stefan.penner@gmail.com>
2017-05-04 13:01:24 -0700

    remove MODEL_FACTORY_INJECTIONS

--- tests/fixtures/brocfile-tests/query/app/app.js
+++ tests/fixtures/brocfile-tests/query/app/app.js
@@ -2,9 +2,7 @@ import Ember from 'ember';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 
-Ember.MODEL_FACTORY_INJECTIONS = true;
-
-var App = Ember.Application.extend({
+const App = Ember.Application.extend({
   modulePrefix: 'query',
   podModulePrefix: 'app/pods',
   Resolver: Resolver
