306d96fee6cfe0e419ee762953c8c3b05ed9b62f
Kelly Selden <kellyselden@gmail.com>
2017-05-25 15:02:04 -0700

    replace phantomjs with firefox

--- tests/fixtures/smoke-tests/js-testem-config/testem.js
+++ tests/fixtures/smoke-tests/js-testem-config/testem.js
@@ -5,10 +5,10 @@ module.exports = {
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
   "launch_in_ci": [
-    "PhantomJS"
+    "Firefox"
   ],
   "launch_in_dev": [
-    "PhantomJS",
+    "Firefox",
     "Chrome"
   ]
 };
