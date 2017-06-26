59f9aa10bfdd0b16b1b11dc0e5bc82767053fa19
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2017-06-26 21:19:07 +0530

    Correct file to use two spaces

--- tests/fixtures/smoke-tests/js-testem-config/testem.js
+++ tests/fixtures/smoke-tests/js-testem-config/testem.js
@@ -1,21 +1,21 @@
 process.env._TESTEM_CONFIG_JS_RAN = true;
 
 module.exports = {
-    "framework": "qunit",
-    "test_page": "tests/index.html?hidepassed",
-    "disable_watching": true,
-    "launch_in_ci": [
-        "Chrome"
+  "framework": "qunit",
+  "test_page": "tests/index.html?hidepassed",
+  "disable_watching": true,
+  "launch_in_ci": [
+    "Chrome"
+  ],
+  "launch_in_dev": [
+    "Chrome"
+  ],
+  "browser_args": {
+    "Chrome": [
+      "--disable-gpu",
+      "--headless",
+      "--remote-debugging-port=9222",
+      "--window-size=1440,900"
     ],
-    "launch_in_dev": [
-        "Chrome"
-    ],
-    "browser_args": {
-        "Chrome": [
-          "--disable-gpu",
-          "--headless",
-          "--remote-debugging-port=9222",
-          "--window-size=1440,900"
-        ],
-    }
+  }
 };
