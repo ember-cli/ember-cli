afbca0c8a41bcba31d457f0137cd88bb2e53ce4c
Robert Wagner <rwwagner90@gmail.com>
2017-07-16 08:04:47 -0400

    Convert blueprints to use modules and bump ember-cli-babel

--- blueprints/app/files/app/app.js
+++ blueprints/app/files/app/app.js
@@ -1,9 +1,9 @@
-import Ember from 'ember';
+import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
-const App = Ember.Application.extend({
+const App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver
--- blueprints/app/files/tests/helpers/module-for-acceptance.js
+++ blueprints/app/files/tests/helpers/module-for-acceptance.js
@@ -1,9 +1,7 @@
 import { module } from 'qunit';
-import Ember from 'ember';
+import { resolve } from 'rsvp';
 import startApp from '../helpers/start-app';
 import destroyApp from '../helpers/destroy-app';
-
-const { RSVP: { resolve } } = Ember;
 
 export default function(name, options = {}) {
   module(name, {
