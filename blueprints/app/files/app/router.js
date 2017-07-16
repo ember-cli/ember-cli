4975c8ab32ea99cf907fbcd5fd5176f83973b364
Robert Wagner <rwwagner90@gmail.com>
2017-07-16 08:22:37 -0400

    EmberRouter

--- blueprints/app/files/app/router.js
+++ blueprints/app/files/app/router.js
@@ -1,7 +1,7 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-const Router = Router.extend({
+const Router = EmberRouter.extend({
   location: config.locationType,
   rootURL: config.rootURL
 });
