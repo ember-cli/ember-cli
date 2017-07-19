ac661e0d48de77c02d829e05cea0813e917c9da8
Alex Navasardyan <anavasardyan@linkedin.com>
2017-07-19 12:18:43 -0700

    SAVEPOINT

--- tests/acceptance/assembler/acceptance-test.js
+++ tests/acceptance/assembler/acceptance-test.js
@@ -5,9 +5,50 @@ const expect = require('../../chai').expect;
 const broccoli = require('broccoli-builder');
 const fixturify = require('fixturify');
 const Assembler = require('../../../lib/assembler');
+const Fixturify = require('broccoli-fixturify');
 const MockProject = require('../../helpers/mock-project');
 
 describe('Acceptance: Assembler', function() {
+  describe('application templates tree', function() {
+    const project = new MockProject();
+    project.root = path.join(process.cwd(), 'tests/fixtures/assembler/app-tree');
+
+    let assembler = new Assembler({
+      name: 'better-errors',
+      env: 'development',
+      tests: true,
+      project,
+      trees: {
+        templates: new Fixturify({
+          'application.hbs': '',
+          'index.hbs': '',
+        }),
+      },
+      registry: {
+        extensionsForType() {
+          return ['hbs'];
+        },
+      },
+    });
+
+    let b = new broccoli.Builder(assembler.getAppTemplatesTree());
+
+    it('works', function() {
+      return b.build().then(options => {
+        let configurationTree = fixturify.readSync(options.directory);
+
+        expect(configurationTree).to.deep.equal({
+          'better-errors': {
+            templates: {
+              'application.hbs': '',
+              'index.hbs': '',
+            },
+          },
+        });
+      });
+    });
+  });
+
   describe('configuration tree', function() {
     const project = new MockProject();
     project.root = path.join(process.cwd(), 'tests/fixtures/assembler/config-tree');
--- tests/unit/assembler/assembler-test.js
+++ tests/unit/assembler/assembler-test.js
@@ -16,32 +16,44 @@ describe('Unit: Assembler', function() {
         env: 'development',
         tests: true,
         project,
+        registry: {},
+        tests: [],
       });
 
       expect(assembler.name).to.equal('better-errors');
       expect(assembler.env).to.equal('development');
       expect(assembler.tests).to.equal(true);
       expect(assembler.project).to.deep.equal(project);
+      expect(assembler.trees).to.be.defined;
+      expect(assembler.registry).to.be.defined;
     });
   });
 
   describe('cache', function() {
+    const project = new MockProject();
+    project.root = path.join(process.cwd(), 'tests/fixtures/assembler/config-tree');
+
+    let assembler = new Assembler({
+      name: 'better-errors',
+      env: 'development',
+      tests: true,
+      project,
+    });
+
     it('configuration tree is cached upon calling `getConfigTree`', function() {
-      const project = new MockProject();
-      project.root = path.join(process.cwd(), 'tests/fixtures/assembler/config-tree');
-
-      let assembler = new Assembler({
-        name: 'better-errors',
-        env: 'development',
-        tests: true,
-        project,
-      });
-
       expect(assembler._cachedConfigTree).to.be.null;
 
       assembler.getConfigTree();
 
       expect(assembler._cachedConfigTree).to.be.defined;
+    });
+
+    it('templates tree is cached upon calling `getAppTemplatesTree`', function() {
+      expect(assembler._cachedTemplatesTree).to.be.null;
+
+      assembler.getAppTemplatesTree();
+
+      expect(assembler._cachedTemplatesTree).to.be.defined;
     });
   });
 });
