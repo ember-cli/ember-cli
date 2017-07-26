6297e7d30a80b7ebad60885fcfc96a60e0882974
Ruslan Grabovoy <kudgo.test@gmail.com>
2017-07-26 22:49:12 +0300

    increase test timeout (－‸ლ)

--- tests/acceptance/install-test-slow.js
+++ tests/acceptance/install-test-slow.js
@@ -14,7 +14,7 @@ let expect = chai.expect;
 let file = chai.file;
 
 describe('Acceptance: ember install', function() {
-  this.timeout(60000);
+  this.timeout(1800000);
 
   beforeEach(co.wrap(function *() {
     let tmpdir = yield mkTmpDirIn(tmproot);
