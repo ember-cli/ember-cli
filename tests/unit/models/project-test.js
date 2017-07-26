4ac6b1de47ce736b85233f3bf34fec1648195e7d
Ricardo Mendes <rokusu@gmail.com>
2017-07-26 04:42:24 +0100

    Fix "SilentError: "

--- tests/unit/models/project-test.js
+++ tests/unit/models/project-test.js
@@ -397,7 +397,7 @@ describe('models/project.js', function() {
 
       const invalidAddonName = 'ember-invalid-addon';
       const expectedPath = path.join(projectPath, '/lib/', invalidAddonName);
-      const expectedError = `SilentError: An error occurred in the constructor for ${invalidAddonName} at ${expectedPath}`;
+      const expectedError = `An error occurred in the constructor for ${invalidAddonName} at ${expectedPath}`;
 
       expect(function() {
         project.initializeAddons();
