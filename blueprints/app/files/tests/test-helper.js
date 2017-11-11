4d21f92ddf2b25cbae2d9326d7b83125bb8e3173
Robert Jackson <rjackson@linkedin.com>
2017-11-11 14:40:06 -0500

    Update to ember-cli-qunit@4.1.1.
    * Replace beta version with stable release.
    * Remove unused `tests/helpers/resolver.js`
    * Import and use `setApplication` directly from `@ember/test-helpers`

--- blueprints/app/files/tests/test-helper.js
+++ blueprints/app/files/tests/test-helper.js
@@ -1,8 +1,7 @@
-import resolver from './helpers/resolver';
-import {
-  setResolver
-} from 'ember-qunit';
-import { start } from 'ember-cli-qunit';
+import Application from '../app';
+import { setApplication } from '@ember/test-helpers';
+import { start } from 'ember-qunit';
 
-setResolver(resolver);
+setApplication(Application.create({ autoboot: false }));
+
 start();
