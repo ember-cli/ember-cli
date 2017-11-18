a419740f25d8ac8de008ef021e4d2e70ca076f9e
Robert Jackson <rjackson@linkedin.com>
2017-11-18 12:28:55 -0500

    Add prettier to default linting configuration.
    `prettier` has taken the JavaScript ecosystem by storm! It has quickly
    become the most common way to acheive "standard code style" without
    being nit-picky with linting rules. The configuration that is landed
    here integrates `prettier` into `eslint` so that `eslint` properly
    identifies things that are not "pretty" and a quick `eslint --fix` will
    auto-correct them. This also means that any editors with built-in
    `eslint --fix` support already support `prettier`.
    
    (cherry picked from commit 1e445a3939e93ab071e96658c423b6801488d848)

--- blueprints/addon/files/addon-config/environment.js
+++ blueprints/addon/files/addon-config/environment.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = function(/* environment, appConfig */) {
-  return { };
+  return {};
 };
--- tests/fixtures/addon/npm/config/ember-try.js
+++ tests/fixtures/addon/npm/config/ember-try.js
@@ -4,71 +4,71 @@ module.exports = {
       name: 'ember-lts-2.12',
       npm: {
         devDependencies: {
-          'ember-source': '~2.12.0'
-        }
-      }
+          'ember-source': '~2.12.0',
+        },
+      },
     },
     {
       name: 'ember-lts-2.16',
       npm: {
         devDependencies: {
-          'ember-source': '~2.16.0'
-        }
-      }
+          'ember-source': '~2.16.0',
+        },
+      },
     },
     {
       name: 'ember-release',
       bower: {
         dependencies: {
-          'ember': 'components/ember#release'
+          ember: 'components/ember#release',
         },
         resolutions: {
-          'ember': 'release'
-        }
+          ember: 'release',
+        },
       },
       npm: {
         devDependencies: {
-          'ember-source': null
-        }
-      }
+          'ember-source': null,
+        },
+      },
     },
     {
       name: 'ember-beta',
       bower: {
         dependencies: {
-          'ember': 'components/ember#beta'
+          ember: 'components/ember#beta',
         },
         resolutions: {
-          'ember': 'beta'
-        }
+          ember: 'beta',
+        },
       },
       npm: {
         devDependencies: {
-          'ember-source': null
-        }
-      }
+          'ember-source': null,
+        },
+      },
     },
     {
       name: 'ember-canary',
       bower: {
         dependencies: {
-          'ember': 'components/ember#canary'
+          ember: 'components/ember#canary',
         },
         resolutions: {
-          'ember': 'canary'
-        }
+          ember: 'canary',
+        },
       },
       npm: {
         devDependencies: {
-          'ember-source': null
-        }
-      }
+          'ember-source': null,
+        },
+      },
     },
     {
       name: 'ember-default',
       npm: {
-        devDependencies: {}
-      }
-    }
-  ]
+        devDependencies: {},
+      },
+    },
+  ],
 };
