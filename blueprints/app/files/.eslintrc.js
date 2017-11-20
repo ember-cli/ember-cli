01337eb98b17d98ff26e2d5f127683572943daa9
Robert Jackson <rjackson@linkedin.com>
2017-11-20 08:58:19 -0500

    Add eslint-plugin-ember to default linting config.

--- blueprints/app/files/.eslintrc.js
+++ blueprints/app/files/.eslintrc.js
@@ -4,8 +4,8 @@ module.exports = {
     ecmaVersion: 2017,
     sourceType: 'module',
   },
-  extends: ['eslint:recommended', 'prettier'],
-  plugins: ['prettier'],
+  extends: ['eslint:recommended', 'prettier', 'plugin:ember/recommended'],
+  plugins: ['prettier', 'ember'],
   env: {
     browser: true,
   },
