26b3c3574a96010091117001dbaf5f45c34a897e
Ruslan Grabovoy <kudgo.test@gmail.com>
2017-12-12 23:43:33 +0200

    reuse app blueprint files in MU blueprint

--- blueprints/module-unification-app/files/tests/test-helper.js
+++
@@ -1,7 +0,0 @@
-import Application from '../src/main';
-import { setApplication } from '@ember/test-helpers';
-import { start } from 'ember-qunit';
-
-setApplication(Application.create({ autoboot: false }));
-
-start();
