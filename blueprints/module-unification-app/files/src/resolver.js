6df5d053b4c1b6b95c05492397e550a6faf1cee6
Gavin Joyce <gavinjoyce@gmail.com>
2018-01-02 12:19:01 +0000

    use `glimmer-wrapper` resolver by default for module unification apps

--- blueprints/module-unification-app/files/src/resolver.js
+++ blueprints/module-unification-app/files/src/resolver.js
@@ -1,4 +1,4 @@
-import Resolver from 'ember-resolver/resolvers/fallback';
+import Resolver from 'ember-resolver/resolvers/glimmer-wrapper';
 import buildResolverConfig from 'ember-resolver/ember-config';
 import config from '../config/environment';
 
