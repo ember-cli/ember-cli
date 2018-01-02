d3d90a77fbd3d51215a4e6a060c4950f5f208d9b
Matthew Beale <matt.beale@madhatted.com>
2018-01-02 15:15:25 -0800

    [BUGFIX release] Dummy app files follow Ember rules
    (cherry picked from commit 422baa888f8b79481014416aff60678ab76c842b)

--- tests/fixtures/addon/npm/.eslintrc.js
+++ tests/fixtures/addon/npm/.eslintrc.js
@@ -28,7 +28,8 @@ module.exports = {
       ],
       excludedFiles: [
         'app/**',
-        'addon/**'
+        'addon/**',
+        'tests/dummy/app/**'
       ],
       parserOptions: {
         sourceType: 'script',
