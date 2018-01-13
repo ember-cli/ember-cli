a5da4fe846bf72a6dccafa9b5e3fe69b14e410ed
Robert Jackson <rjackson@linkedin.com>
2018-01-13 00:01:23 -0500

    Make ember-try a direct project dependency.
    Removes ember-try from being a dependency of ember-cli itself, and makes
    it a dependency in the default addon blueprint.
    
    This allows ember-try to iterate and bump major versions without having
    to be concerned with "can we deliver urgent bugfixes to our actual
    users".
    
    It also reduces the "package weight" of ember-cli itself (and for normal
    apps)...

--- tests/unit/blueprints/addon-test.js
+++ tests/unit/blueprints/addon-test.js
@@ -76,6 +76,7 @@ describe('blueprint - addon', function() {
   "dependencies": {},\n\
   "devDependencies": {\n\
     "ember-disable-prototype-extensions": "^1.1.2",\n\
+    "ember-try": "^0.2.22",\n\
     "eslint-plugin-node": "^5.2.1"\n\
   },\n\
   "ember-addon": {\n\
@@ -172,6 +173,7 @@ describe('blueprint - addon', function() {
         let json = JSON.parse(output);
         delete json.devDependencies['ember-disable-prototype-extensions'];
         delete json.devDependencies['eslint-plugin-node'];
+        delete json.devDependencies['ember-try'];
         expect(json.dependencies).to.deep.equal({ a: "1", b: "1" });
         expect(json.devDependencies).to.deep.equal({ a: "1", b: "1" });
       });
