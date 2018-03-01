6c64cf17155688f7086dbb0490da15417c8e9b41
Alex Navasardyan <alex.navasardyan@gmail.com>
2018-02-28 22:28:02 -0800

    Extract `contentFor` into `ember-app-utils`
    Main motivation behind this change is on-going extraction into default
    packager. Because, we previously bound `contentFor` to application's
    context, it was harder to extract the way we assemble certain trees
    (config tree, for example).
    
    This change does the following:
    
    + removes that binding and replaces it with explicit contract (we pass
    now pass in additional `options` object) which makes it easier to test
    (no need to set up `EmberApp`) and easier to reason about
    + adds missing tests for both "app-boot" and "test-body-footer"
    types
    + removes `contentFor` method from `EmberApp`: not a breaking change
    since it is a private method

--- tests/unit/utilities/ember-app-utils-test.js
+++ tests/unit/utilities/ember-app-utils-test.js
@@ -1,14 +1,224 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const expect = require('chai').expect;
 
 const emberAppUtils = require('../../../lib/utilities/ember-app-utils');
 
+const contentFor = emberAppUtils.contentFor;
 const normalizeUrl = emberAppUtils.normalizeUrl;
 const calculateBaseTag = emberAppUtils.calculateBaseTag;
 const convertObjectToString = emberAppUtils.convertObjectToString;
 
 describe('ember-app-utils', function() {
+  describe(`contentFor`, function() {
+    let config = {
+      modulePrefix: 'cool-foo',
+    };
+
+    let defaultMatch = '{{content-for \'head\'}}';
+    let escapedConfig = escape(JSON.stringify(config));
+    let defaultOptions = {
+      storeConfigInMeta: true,
+      autoRun: true,
+      addons: [],
+      isModuleUnification: false,
+    };
+
+    it('returns an empty string if invalid type is specified', function() {
+      expect(contentFor(config, defaultMatch, 'foo', defaultOptions)).to.equal('');
+      expect(contentFor(config, defaultMatch, 'body', defaultOptions)).to.equal('');
+      expect(contentFor(config, defaultMatch, 'blah', defaultOptions)).to.equal('');
+    });
+
+    describe('"head"', function() {
+      it('returns `<meta>` tag by default', function() {
+        let actual = contentFor(config, defaultMatch, 'head', defaultOptions);
+        let expected = `<meta name="cool-foo/config/environment" content="${escapedConfig}" />`;
+
+        expect(
+          actual,
+          '`<meta>` tag was included by default'
+        ).to.contain(expected);
+      });
+
+      it('omits `<meta>` tag if `storeConfigInMeta` is false', function() {
+        let options = Object.assign({}, defaultOptions, { storeConfigInMeta: false });
+
+        let output = contentFor(config, defaultMatch, 'head', options);
+        let expected = `<meta name="cool-foo/config/environment" content="${escapedConfig}" />`;
+
+        expect(
+          output,
+          '`<meta>` tag was not included'
+        ).not.to.contain(expected);
+      });
+
+      it('returns `<base>` tag if `locationType` is "auto"', function() {
+        config.locationType = 'auto';
+        config.baseURL = '/';
+
+        let expected = '<base href="/" />';
+        let output = contentFor(config, defaultMatch, 'head', defaultOptions);
+
+        expect(
+          output,
+          '`<base>` tag was included'
+        ).to.contain(expected);
+      });
+
+      // this is required by testem
+      it('returns `<base>` tag if `locationType` is "none"', function() {
+        config.locationType = 'none';
+        config.baseURL = '/';
+
+        let output = contentFor(config, defaultMatch, 'head', defaultOptions);
+        let expected = '<base href="/" />';
+
+        expect(
+          output,
+          '`<base>` tag was included'
+        ).to.contain(expected);
+      });
+
+      it('omits `<base>` tag if `locationType` is "hash"', function() {
+        config.locationType = 'hash';
+        config.baseURL = '/foo/bar';
+
+        let expected = '<base href="/foo/bar/" />';
+        let output = contentFor(config, defaultMatch, 'head', defaultOptions);
+
+        expect(
+          output,
+          '`<base>` tag was not included'
+        ).to.not.contain(expected);
+      });
+
+      it('omits `<base>` tag if `baseURL` is `undefined`', function() {
+        let expected = '<base href=';
+        let output = contentFor(config, defaultMatch, 'head', defaultOptions);
+
+        expect(
+          output,
+          '`<base>` tag was not included'
+        ).to.not.contain(expected);
+      });
+    });
+
+    describe('"config-module"', function() {
+      it('returns `<meta>` tag gathering snippet by default', function() {
+        let metaSnippetPath = path.join(__dirname, '..', '..', '..', 'lib', 'broccoli', 'app-config-from-meta.js');
+        let expected = fs.readFileSync(metaSnippetPath, { encoding: 'utf8' });
+
+        let output = contentFor(config, defaultMatch, 'config-module', defaultOptions);
+
+        expect(
+          output,
+          'includes `<meta>` tag snippet'
+        ).to.contain(expected);
+      });
+
+      it('returns "raw" config if `storeConfigInMeta` is false', function() {
+        let options = Object.assign({}, defaultOptions, { storeConfigInMeta: false });
+        let expected = JSON.stringify(config);
+        let output = contentFor(config, defaultMatch, 'config-module', options);
+
+        expect(
+          output,
+          'includes "raw" config'
+        ).to.contain(expected);
+      });
+    });
+
+    describe('"app-boot"', function() {
+      it('returns application bootstrap snippet by default', function() {
+        let output = contentFor(config, defaultMatch, 'app-boot', defaultOptions);
+
+        expect(
+          output,
+          'includes applicaton bootstrap snippet'
+        ).to.contain('require("cool-foo/app")["default"].create({});');
+      });
+
+      it('returns application bootstrap snippet with MU module name if `isModuleUnification` is true', function() {
+        let options = Object.assign({}, defaultOptions, { isModuleUnification: true });
+        let output = contentFor(config, defaultMatch, 'app-boot', options);
+
+        expect(
+          output,
+          'includes applicaton bootstrap snippet'
+        ).to.contain('require("cool-foo/src/main")["default"].create({});');
+      });
+
+      it('omits application bootstrap snippet if `autoRun` is false', function() {
+        let options = Object.assign({}, defaultOptions, { autoRun: false });
+        let output = contentFor(config, defaultMatch, 'app-boot', options);
+
+        expect(
+          output,
+          'includes applicaton bootstrap snippet'
+        ).to.equal('');
+      });
+    });
+
+    describe('"test-body-footer"', function() {
+      it('returns `<script> tag with a failed test load assertion`', function() {
+        let output = contentFor(config, defaultMatch, 'test-body-footer', defaultOptions);
+
+        expect(
+          output,
+          'includes `<script>` tag'
+        ).to.equal(`<script>Ember.assert('The tests file was not loaded. Make sure your tests index.html includes "assets/tests.js".', EmberENV.TESTS_FILE_LOADED);</script>`);
+      });
+    });
+
+    describe(`for addons`, function() {
+      it('allows later addons to inspect previous content', function() {
+        let calledContent;
+        let addons = [{
+          contentFor() {
+            return 'zero';
+          },
+        }, {
+          contentFor() {
+            return 'one';
+          },
+        }, {
+          contentFor(type, config, content) {
+            calledContent = content.slice();
+            content.pop();
+
+            return 'two';
+          },
+        }];
+
+        let options = Object.assign({}, defaultOptions, { addons });
+        let output = contentFor(config, defaultMatch, 'foo', options);
+
+        expect(calledContent).to.deep.equal(['zero', 'one']);
+        expect(output).to.equal('zero\ntwo');
+      });
+
+      it('calls `contentFor` on addons', function() {
+        let addons = [{
+          contentFor() {
+            return 'blammo';
+          },
+        }, {
+          contentFor() {
+            return 'blahzorz';
+          },
+        }];
+
+        let options = Object.assign({}, defaultOptions, { addons });
+        let output = contentFor(config, defaultMatch, 'foo', options);
+
+        expect(output).to.equal('blammo\nblahzorz');
+      });
+    });
+  });
+
   describe(`calculateBaseTag`, function() {
     ['auto', 'history'].forEach(locationType => {
       it(`generates a base tag correctly for location: ${locationType}`, function() {
