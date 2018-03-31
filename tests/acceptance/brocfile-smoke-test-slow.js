c3bc9349abf38772b1883dd5d2d3c7c52bebe4ba
billybonks <sebastienstettler@gmail.com>
2018-03-31 10:59:05 +0800

    assert no filters matched

--- tests/acceptance/brocfile-smoke-test-slow.js
+++ tests/acceptance/brocfile-smoke-test-slow.js
@@ -168,6 +168,7 @@ describe('Acceptance: brocfile-smoke-test', function() {
   }));
 
   it('addon trees are not jshinted', co.wrap(function *() {
+    let assertions = 0;
     yield copyFixtureFiles('brocfile-tests/jshint-addon');
 
     let packageJsonPath = path.join(appRoot, 'package.json');
@@ -176,8 +177,14 @@ describe('Acceptance: brocfile-smoke-test', function() {
       paths: ['./lib/ember-random-thing'],
     };
     fs.writeJsonSync(packageJsonPath, packageJson);
-
-    yield runCommand(path.join('.', 'node_modules', 'ember-cli', 'bin', 'ember'), 'test', '--filter=jshint');
+    try {
+      yield runCommand(path.join('.', 'node_modules', 'ember-cli', 'bin', 'ember'), 'test', '--filter=jshint');
+    } catch (e) {
+      let outputMatch = e.output.join(' ').match(/(No tests matched the filter "jshint")/g).length;
+      chai.expect(outputMatch).to.equal(3);
+      assertions++;
+    }
+    chai.expect(assertions).to.equal(1);
   }));
 
   it('specifying custom output paths works properly', co.wrap(function *() {
