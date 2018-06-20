9dbd15ef223889db6c49899cb6f8ed641cc9cafe
Stefan Penner <stefan.penner@gmail.com>
2018-06-20 15:27:03 -0700

    Update testem.js
    Some better testem config defaults for headless chrome
    
    Context for --disable-dev-shm-usage -> https://bugs.chromium.org/p/chromium/issues/detail?id=736452#c64

--- blueprints/app/files/testem.js
+++ blueprints/app/files/testem.js
@@ -12,9 +12,11 @@ module.exports = {
       ci: [
         // --no-sandbox is needed when running Chrome inside a container
         process.env.CI ? '--no-sandbox' : null,
-
-        '--disable-gpu',
         '--headless',
+        '--disable-gpu',
+        '--disable-dev-shm-usage',
+        '--disable-software-rasterizer',
+        '--mute-audio',
         '--remote-debugging-port=0',
         '--window-size=1440,900'
       ].filter(Boolean)
