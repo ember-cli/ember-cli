3a957da77d205e0981e4040608cb24c1a7c804db
Pepe Cano <825430+ppcano@users.noreply.github.com>
2018-12-06 14:51:19 +0100

    Add MU preprocessor smoke tests

---
+++ tests/fixtures/preprocessor-tests/mu-app-registry-ordering/src/ui/components/should-be-preprocessed.js
@@ -0,0 +1,5 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  foo: /__SECOND_PREPROCESSOR_REPLACEMENT_TOKEN__/
+});
---
+++ tests/fixtures/preprocessor-tests/mu-app-with-addon-with-preprocessors-2/node_modules/ember-cool-addon/addon/components/clitest-example.js
@@ -0,0 +1,5 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  foo: __PREPROCESSOR_REPLACEMENT_TOKEN__
+});
