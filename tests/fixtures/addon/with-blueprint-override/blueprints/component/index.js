faf7de3d63ed88467100c3d3216462ad349cf166
Pepe Cano <825430+ppcano@users.noreply.github.com>
2018-12-07 12:32:58 +0100

    Support skipped MU Acceptance smoke tests

--- tests/fixtures/addon/with-blueprint-override/blueprints/component/index.js
+++ tests/fixtures/addon/with-blueprint-override/blueprints/component/index.js
@@ -1,4 +1,38 @@
+const { isExperimentEnabled } = require('../../../../lib/experiments');
+const path = require('path');
+
+
 module.exports = {
+
+  filesPath: function() {
+    let filesDirectory = 'files';
+
+    if (isExperimentEnabled('MODULE_UNIFICATION')) {
+      filesDirectory = 'module-unification-files';
+    }
+
+    return path.join(this.path, filesDirectory);
+  },
+
+  fileMapTokens: function() {
+    if (isExperimentEnabled('MODULE_UNIFICATION')) {
+      return {
+        __root__(options) {
+          return 'src';
+        },
+        __path__(options) {
+          return path.join('ui', 'components', 'new-path', options.dasherizedModuleName);
+        },
+      };
+    } else {
+      return {
+        __path__: function(options) {
+          return path.join('components', 'new-path');
+        },
+      };
+    }
+  },
+
   locals(options) {
     return {
       importTemplate: '',
---
+++ tests/fixtures/addon/with-blueprint-override/blueprints/component/module-unification-files/__root__/__path__/component.js
@@ -0,0 +1,6 @@
+import Component from '@ember/component';
+<%= importTemplate %>
+
+export default Component.extend({<%= contents %>
+});
+//generated component successfully
