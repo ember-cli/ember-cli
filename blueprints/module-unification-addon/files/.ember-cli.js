bb748d31e07fd0420447f0c62af0d039593e16ac
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-12-24 21:56:00 -0500

    change MU addon ember-cli to .js

--- blueprints/module-unification-addon/files/.ember-cli.js
+++ blueprints/module-unification-addon/files/.ember-cli.js
@@ -1,4 +1,8 @@
-{
+'use strict';
+
+process.env.EMBER_CLI_MODULE_UNIFICATION = true;
+
+module.exports = {
   /**
     Ember CLI sends analytics information by default. The data is completely
     anonymous, but there are times when you might want to disable this behavior.
