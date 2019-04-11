ac6efc3da44544677915f8a10db3cf1d2f1608d8
Richard Otvos <richard.otvos@cloudera.com>
2019-04-11 15:52:11 +0200

    #8511 Modernizes unit/blueprint/packages-test.js

--- tests/unit/blueprints/packages-test.js
+++ tests/unit/blueprints/packages-test.js
@@ -15,28 +15,25 @@ describe('Acceptance: ember generate and destroy packages', function() {
     cliPath: path.resolve(`${__dirname}/../../..`),
   });
 
-  it('packages foo', function() {
+  it('packages foo', async function() {
     let args = ['packages', 'foo'];
 
-    return emberNew().then(() =>
-      emberGenerateDestroy(args, file => {
-        expect(dir('packages')).to.exist;
-        expect(file('lib/.eslintrc.js')).to.not.exist;
-        expect(file('packages/.jshintrc')).to.not.exist;
-      })
-    );
+    await emberNew();
+    await emberGenerateDestroy(args, file => {
+      expect(dir('packages')).to.exist;
+      expect(file('lib/.eslintrc.js')).to.not.exist;
+      expect(file('packages/.jshintrc')).to.not.exist;
+    });
   });
 
-  it('packages foo with ember-cli-jshint', function() {
+  it('packages foo with ember-cli-jshint', async function() {
     let args = ['packages', 'foo'];
 
-    return emberNew()
-      .then(() => modifyPackages([{ name: 'ember-cli-jshint', dev: true }]))
-      .then(() =>
-        emberGenerateDestroy(args, file => {
-          expect(dir('packages')).to.exist;
-          expect(file('packages/.jshintrc')).to.not.exist;
-        })
-      );
+    await emberNew();
+    await modifyPackages([{ name: 'ember-cli-jshint', dev: true }]);
+    emberGenerateDestroy(args, file => {
+      expect(dir('packages')).to.exist;
+      expect(file('packages/.jshintrc')).to.not.exist;
+    });
   });
 });
