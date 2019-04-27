a51febb7b30b3ae591368278a20c0f800ffb73c7
Oli Griffiths <oligriffiths@users.noreply.github.com>
2019-04-27 12:11:27 -0400

    Add experiemnt

--- tests/unit/models/builder-test.js
+++ tests/unit/models/builder-test.js
@@ -284,39 +284,47 @@ describe('models/builder.js', function() {
       });
     });
 
-    it('builds an esm ember-cli-build.js', function() {
-      const project = new MockProject();
-      project.root += '/tests/fixtures/build/esm';
+    if (isExperimentEnabled('ESM_TYPESCRIPT')) {
+      it('builds an esm ember-cli-build.js', function () {
+        const project = new MockProject();
+        project.root += '/tests/fixtures/build/esm';
 
-      builder = new Builder({
-        project,
-        ui: project.ui,
-        processBuildResult(buildResults) { return Promise.resolve(buildResults); },
+        builder = new Builder({
+          project,
+          ui: project.ui,
+          processBuildResult(buildResults) {
+            return Promise.resolve(buildResults);
+          },
+        });
+
+        return builder.build().then(function (result) {
+          expect(fs.existsSync(result.directory)).to.be.true;
+          expect(fs.existsSync(path.join(result.directory, 'hello.txt'))).to.be.true;
+          expect(fs.readFileSync(path.join(result.directory, 'hello.txt'), 'utf-8')).to.equal(`Hello esm\n`);
+        });
       });
+    }
 
-      return builder.build().then(function(result) {
-        expect(fs.existsSync(result.directory)).to.be.true;
-        expect(fs.existsSync(path.join(result.directory, 'hello.txt'))).to.be.true;
-        expect(fs.readFileSync(path.join(result.directory, 'hello.txt'), 'utf-8')).to.equal(`Hello esm\n`);
+    if (isExperimentEnabled('ESM_TYPESCRIPT')) {
+      it('builds a typescript ember-cli-build.ts', function () {
+        const project = new MockProject();
+        project.root += '/tests/fixtures/build/typescript';
+
+        builder = new Builder({
+          project,
+          ui: project.ui,
+          processBuildResult(buildResults) {
+            return Promise.resolve(buildResults);
+          },
+        });
+
+        return builder.build().then(function (result) {
+          expect(fs.existsSync(result.directory)).to.be.true;
+          expect(fs.existsSync(path.join(result.directory, 'hello.txt'))).to.be.true;
+          expect(fs.readFileSync(path.join(result.directory, 'hello.txt'), 'utf-8')).to.equal(`Hello typescript\n`);
+        });
       });
-    });
-
-    it('builds a typescript ember-cli-build.ts', function() {
-      const project = new MockProject();
-      project.root += '/tests/fixtures/build/typescript';
-
-      builder = new Builder({
-        project,
-        ui: project.ui,
-        processBuildResult(buildResults) { return Promise.resolve(buildResults); },
-      });
-
-      return builder.build().then(function(result) {
-        expect(fs.existsSync(result.directory)).to.be.true;
-        expect(fs.existsSync(path.join(result.directory, 'hello.txt'))).to.be.true;
-        expect(fs.readFileSync(path.join(result.directory, 'hello.txt'), 'utf-8')).to.equal(`Hello typescript\n`);
-      });
-    });
+    }
   });
 
   describe('cleanup', function() {
