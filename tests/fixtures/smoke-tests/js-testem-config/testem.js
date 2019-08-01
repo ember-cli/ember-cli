668ea503e97bd148081f742d271d44c0f5fa836c
Tom Dale <tom@tomdale.net>
2019-08-01 12:53:02 -0400

    Remove --disable-gpu flag from testem config blueprints
    This flag was originally required by headless Chrome when running on Windows. However, now that https://bugs.chromium.org/p/chromium/issues/detail?id=729961 has landed, this flag is no longer need on Mac, Linux, or Windows.

--- tests/fixtures/smoke-tests/js-testem-config/testem.js
+++ tests/fixtures/smoke-tests/js-testem-config/testem.js
@@ -15,7 +15,6 @@ module.exports = {
       // --no-sandbox is needed when running Chrome inside a container
       process.env.TRAVIS ? '--no-sandbox' : null,
 
-      "--disable-gpu",
       "--headless",
       "--remote-debugging-port=0",
       "--window-size=1440,900"
