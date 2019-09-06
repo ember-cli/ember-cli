c40be1ab1e1a431669dfa06065c71dc33c412524
Cyrille David <dcyriller@gmail.com>
2019-09-06 19:23:19 +0200

    cleanup: Remove MU fixtures

--- tests/fixtures/addon/kitchen-sink-mu/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: require('./package').name,
-
-  contentFor(type, config) {
-    if (type === 'head') {
-      return '"SOME AWESOME STUFF"';
-    }
-  }
-};
--- tests/fixtures/addon/kitchen-sink-mu/tests/dummy/src/ui/components/second-thing/component.js
+++
@@ -1,5 +0,0 @@
-import BasicThing from 'some-cool-addon/src/ui/components/basic-thing/component';
-
-export default BasicThing.extend({
-  classNames: ['second-thing']
-});
