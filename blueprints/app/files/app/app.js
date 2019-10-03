ea1a067e3549aaa49fa7c3d76baea00fa6a46ded
Panman8201 <panman82@gmail.com>
2019-10-03 11:35:24 -0500

    Sync app and addon blueprint from octane blueprints
    Diff'ed the output between the ember-cli addon blueprint with the octane addon blueprint. Only found a few slight differences, included in this commit.

--- blueprints/app/files/app/app.js
+++ blueprints/app/files/app/app.js
@@ -3,12 +3,10 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
-const App = Application.extend({
-  modulePrefix: config.modulePrefix,
-  podModulePrefix: config.podModulePrefix,
-  Resolver
-});
+export default class App extends Application {
+  modulePrefix = config.modulePrefix;
+  podModulePrefix = config.podModulePrefix;
+  Resolver = Resolver;
+}
 
 loadInitializers(App, config.modulePrefix);
-
-export default App;
