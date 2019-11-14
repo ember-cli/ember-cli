7cfcc4eaa1f64d415d72e69ed6642edad9763714
Jan Buschtöns <jan@buschtoens.me>
2019-11-14 17:54:07 +0100

    feat(blueprint): allow cloning git repo with ref

--- tests/unit/utilities/git-repo-test.js
+++ tests/unit/utilities/git-repo-test.js
@@ -5,9 +5,16 @@ const expect = require('chai').expect;
 
 describe('is-git-url', function() {
   it('recognizes git-style urls in various formats', function() {
+    // without ref
     expect(isGitRepo('https://github.com/trek/app-blueprint-test.git')).to.be.ok;
     expect(isGitRepo('git@github.com:trek/app-blueprint-test.git')).to.be.ok;
     expect(isGitRepo('git+ssh://user@server/project.git')).to.be.ok;
     expect(isGitRepo('git+https://user@server/project.git')).to.be.ok;
+
+    // with ref
+    expect(isGitRepo('https://github.com/trek/app-blueprint-test.git#named-ref')).to.be.ok;
+    expect(isGitRepo('git@github.com:trek/app-blueprint-test.git#named-ref')).to.be.ok;
+    expect(isGitRepo('git+ssh://user@server/project.git#named-ref')).to.be.ok;
+    expect(isGitRepo('git+https://user@server/project.git#named-ref')).to.be.ok;
   });
 });
