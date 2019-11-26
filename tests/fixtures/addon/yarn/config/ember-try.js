d3fa91affaf676cbaaba162bf7653dc0bb33d81e
Kelly Selden <kellyselden@gmail.com>
2019-11-26 11:12:57 +0000

    replace addon testing ember 3.4 for 3.12

--- tests/fixtures/addon/yarn/config/ember-try.js
+++ tests/fixtures/addon/yarn/config/ember-try.js
@@ -7,18 +7,18 @@ module.exports = async function() {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-3.4',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.4.0'
-          }
-        }
-      },
-      {
         name: 'ember-lts-3.8',
         npm: {
           devDependencies: {
             'ember-source': '~3.8.0'
+          }
+        }
+      },
+      {
+        name: 'ember-lts-3.12',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.12.0'
           }
         }
       },
