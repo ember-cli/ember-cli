5e8d1ad0f018dc7f828da5ec6a78332ea65fcf8d
Robert Jackson <rjackson@linkedin.com>
2020-03-12 11:00:15 -0400

    Avoid attempting to kill an already killed child process.

--- tests/helpers/kill-cli-process.js
+++ tests/helpers/kill-cli-process.js
@@ -1,6 +1,12 @@
 'use strict';
 
 module.exports = function(childProcess) {
+  // Calling `.kill` or `.send` when the process has already exited causes
+  // errors on Windows, when an `.exitCode` is non-null the process has exited
+  if (childProcess.exitCode !== null) {
+    return;
+  }
+
   if (process.platform === 'win32') {
     childProcess.send({ kill: true });
   } else {
