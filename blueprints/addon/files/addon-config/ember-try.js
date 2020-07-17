3a9599a478ec7d5a77aa955fea409002e2f59793
Mehul Kar <mehulkar@apple.com>
2020-07-17 15:32:45 -0700

    Revert "only include default ember-try scenario in CI for addons"
    This reverts commit 79f9744704c336dfc10569f7ff8e10d45952a93a.

--- blueprints/addon/files/addon-config/ember-try.js
+++ blueprints/addon/files/addon-config/ember-try.js
@@ -2,93 +2,88 @@
 
 const getChannelURL = require('ember-source-channel-url');
 
-const scenarios = [
-  {
-    name: 'ember-lts-3.12',
-    npm: {
-      devDependencies: {
-        'ember-source': '~3.12.0'
-      }
-    }
-  },
-  {
-    name: 'ember-lts-3.16',
-    npm: {
-      devDependencies: {
-        'ember-source': '~3.16.0'
-      }
-    }
-  },
-  {
-    name: 'ember-release',
-    npm: {
-      devDependencies: {
-        'ember-source': await getChannelURL('release')
-      }
-    }
-  },
-  {
-    name: 'ember-beta',
-    npm: {
-      devDependencies: {
-        'ember-source': await getChannelURL('beta')
-      }
-    }
-  },
-  {
-    name: 'ember-canary',
-    npm: {
-      devDependencies: {
-        'ember-source': await getChannelURL('canary')
-      }
-    }
-  },
-  {
-    name: 'ember-default-with-jquery',
-    env: {
-      EMBER_OPTIONAL_FEATURES: JSON.stringify({
-        'jquery-integration': true
-      })
-    },
-    npm: {
-      devDependencies: {
-        '@ember/jquery': '^0.5.1'
-      }
-    }
-  },
-  {
-    name: 'ember-classic',
-    env: {
-      EMBER_OPTIONAL_FEATURES: JSON.stringify({
-        'application-template-wrapper': true,
-        'default-async-observers': false,
-        'template-only-glimmer-components': false
-      })
-    },
-    npm: {
-      ember: {
-        edition: 'classic'
-      }
-    }
-  }
-];
-
-// The default `.travis.yml` runs this scenario via `<% if (yarn) { %>yarn<% } else { %>npm<% } %> test`,
-// not via `ember try`, so we don't need it include it in CI. Other than that, we will include it
-// so that running `ember try:each` manually will run it along with all the other scenarios, and
-// ember try:one can still pick up this config.
-if (!process.env.CI) {
-  scenarios.push({
-    name: 'ember-default',
-    npm: {
-      devDependencies: {}
-    }
-  })
-};
-
 module.exports = async function() {
   return {
     <% if (yarn) { %>useYarn: true,
-    <% } %>scenarios: scenarios
+    <% } %>scenarios: [
+      {
+        name: 'ember-lts-3.12',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.12.0'
+          }
+        }
+      },
+      {
+        name: 'ember-lts-3.16',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.16.0'
+          }
+        }
+      },
+      {
+        name: 'ember-release',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('release')
+          }
+        }
+      },
+      {
+        name: 'ember-beta',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('beta')
+          }
+        }
+      },
+      {
+        name: 'ember-canary',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('canary')
+          }
+        }
+      },
+      // The default `.travis.yml` runs this scenario via `<% if (yarn) { %>yarn<% } else { %>npm<% } %> test`,
+      // not via `ember try`. It's still included here so that running
+      // `ember try:each` manually or from a customized CI config will run it
+      // along with all the other scenarios.
+      {
+        name: 'ember-default',
+        npm: {
+          devDependencies: {}
+        }
+      },
+      {
+        name: 'ember-default-with-jquery',
+        env: {
+          EMBER_OPTIONAL_FEATURES: JSON.stringify({
+            'jquery-integration': true
+          })
+        },
+        npm: {
+          devDependencies: {
+            '@ember/jquery': '^0.5.1'
+          }
+        }
+      },
+      {
+        name: 'ember-classic',
+        env: {
+          EMBER_OPTIONAL_FEATURES: JSON.stringify({
+            'application-template-wrapper': true,
+            'default-async-observers': false,
+            'template-only-glimmer-components': false
+          })
+        },
+        npm: {
+          ember: {
+            edition: 'classic'
+          }
+        }
+      }
+    ]
   };
 };
