612c220ad0426b37ead40110e522101906b79b67
Travis Hoover <thoov7@gmail.com>
2021-03-16 19:12:40 -0700

    fixing fixture

--- tests/fixtures/app/embroider/ember-cli-build.js
+++ tests/fixtures/app/embroider/ember-cli-build.js
@@ -21,5 +21,9 @@ module.exports = function (defaults) {
   // along with the exports of each module as its value.
 
   const { Webpack } = require('@embroider/webpack');
-  return require('@embroider/compat').compatBuild(app, Webpack);
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    skipBabel: [{
+      package: 'qunit'
+    }]
+  });
 };
