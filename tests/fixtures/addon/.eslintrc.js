68c6bc4515d9081683e0d3811e95079e905d1a11
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2021-05-10 11:07:45 -0700

    chore: replace deprecated babel-eslint with @babel/eslint-parser
    https://www.npmjs.com/package/@babel/eslint-parser

--- tests/fixtures/addon/.eslintrc.js
+++ tests/fixtures/addon/.eslintrc.js
@@ -2,13 +2,14 @@
 
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
     ecmaFeatures: {
       legacyDecorators: true,
     },
+    requireConfigFile: false,
   },
   plugins: ['ember'],
   extends: [
