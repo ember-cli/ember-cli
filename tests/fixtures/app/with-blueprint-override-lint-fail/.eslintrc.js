b5cbf22007f1740a37bc5791fbc3663f6554d323
Albert Yu <dev.yufan@gmail.com>
2021-10-18 18:56:21 +0800

    update test fixtures for eslint v8

--- tests/fixtures/app/with-blueprint-override-lint-fail/.eslintrc.js
+++ tests/fixtures/app/with-blueprint-override-lint-fail/.eslintrc.js
@@ -2,12 +2,15 @@
 
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
-    ecmaFeatures: {
-      legacyDecorators: true,
+    requireConfigFile: false,
+    babelOptions: {
+      plugins: [
+        ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
+      ],
     },
   },
   plugins: ['ember'],
