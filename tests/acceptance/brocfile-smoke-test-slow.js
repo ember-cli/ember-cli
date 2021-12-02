9fd7dd77bb0424ff4d9177d023fd11137e962cd6
Kelly Selden <kellyselden@gmail.com>
2021-12-02 15:02:19 -0800

    Revert "Skip outputPaths.app.html customizing test."
    This reverts commit 1acc80ace7a96257d9b6ad0d80b4e1b9c2c5e234.

--- tests/acceptance/brocfile-smoke-test-slow.js
+++ tests/acceptance/brocfile-smoke-test-slow.js
@@ -203,8 +203,7 @@ describe('Acceptance: brocfile-smoke-test', function () {
 
   // custom outputPaths are deprecated under embroider
   if (!isExperimentEnabled('EMBROIDER')) {
-    // skipping this as it seems this functionality doesn't work with ember-auto-import@2.2.3
-    it.skip('specifying custom output paths works properly', async function () {
+    it('specifying custom output paths works properly', async function () {
       await copyFixtureFiles('brocfile-tests/custom-output-paths');
 
       let themeCSSPath = path.join(appRoot, 'app', 'styles', 'theme.css');
@@ -230,8 +229,7 @@ describe('Acceptance: brocfile-smoke-test', function () {
     });
   }
 
-  // skipping this as it seems this functionality doesn't work with ember-auto-import@2.2.3
-  it.skip('specifying outputFile results in an explicitly generated assets', async function () {
+  it('specifying outputFile results in an explicitly generated assets', async function () {
     await copyFixtureFiles('brocfile-tests/app-import-output-file');
     await runCommand(path.join('.', 'node_modules', 'ember-cli', 'bin', 'ember'), 'build');
 
@@ -431,8 +429,7 @@ describe('Acceptance: brocfile-smoke-test', function () {
     expect(file(`dist/assets/${appName}.css`)).to.equal('body { background: green; }\n');
   });
 
-  // skipping this as it seems this functionality doesn't work with ember-auto-import@2.2.3
-  it.skip('additional trees can be passed to the app', async function () {
+  it('additional trees can be passed to the app', async function () {
     await copyFixtureFiles('brocfile-tests/additional-trees');
     await runCommand(path.join('.', 'node_modules', 'ember-cli', 'bin', 'ember'), 'build', { verbose: true });
 
