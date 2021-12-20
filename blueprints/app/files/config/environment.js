980dbd3d329ea3f69066baffe71b2c03d80107e6
Katie Gengler <katie@kmg.io>
2021-12-20 15:25:43 -0500

    [BUGFIX beta] Change blueprint to generate apps using 'history' location as 'auto' location has been deprecated
    - Update some test blueprints that do not care about location type

--- blueprints/app/files/config/environment.js
+++ blueprints/app/files/config/environment.js
@@ -5,7 +5,7 @@ module.exports = function (environment) {
     modulePrefix: '<%= modulePrefix %>',
     environment,
     rootURL: '/',
-    locationType: 'auto',
+    locationType: 'history',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
