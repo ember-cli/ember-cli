3ed7320000c9d6a26167b80e8d9e7fea692d7ca5
Chris Freeman <cfreeman.code@gmail.com>
2022-02-03 13:00:52 -0700

    PR feedback and handle `--no-typescript` flag

--- tests/acceptance/typescript-generate-test.js
+++ tests/acceptance/typescript-generate-test.js
@@ -17,7 +17,7 @@ const chai = require('../chai');
 let expect = chai.expect;
 let file = chai.file;
 
-describe('Acceptance: ember generate', function () {
+describe('Acceptance: ember generate with typescript blueprints', function () {
   this.timeout(20000);
 
   let tmpdir;
@@ -130,6 +130,51 @@ describe('Acceptance: ember generate', function () {
     expect(testGenerated).to.contain('export default function bar(a: string, b: number): string {');
   });
 
+  it('outputs typescript {typescript: true} is present in .ember-cli', async function () {
+    await initApp();
+    await fs.writeJSON('.ember-cli', {
+      typescript: true,
+    });
+
+    await outputFile(
+      'blueprints/foo/index.js',
+      `module.exports = {
+        shouldTransformTypeScript: true
+      }`
+    );
+
+    await outputFile(
+      'blueprints/foo/files/app/foos/__name__.ts',
+      `export default function <%= camelizedModuleName %>(a: string, b: number): string {
+        return a + b;
+      }`
+    );
+
+    await outputFile(
+      'blueprints/foo-test/index.js',
+      `module.exports = {
+        shouldTransformTypeScript: true
+      }`
+    );
+
+    await outputFile(
+      'blueprints/foo-test/files/tests/foos/__name__-test.ts',
+      `export default function <%= camelizedModuleName %>(a: string, b: number): string {
+        return a + b;
+      }`
+    );
+
+    await ember(['generate', 'foo', 'bar']);
+
+    const generated = file('app/foos/bar.ts');
+    expect(generated).to.equal(`export default function bar(a: string, b: number): string {
+        return a + b;
+      }`);
+
+    const testGenerated = file('tests/foos/bar-test.ts');
+    expect(testGenerated).to.contain('export default function bar(a: string, b: number): string {');
+  });
+
   it('generates typescript when the `--typescript` flag is used and a typescript blueprint exists', async function () {
     await initApp();
 
@@ -177,6 +222,54 @@ describe('Acceptance: ember generate', function () {
     expect(output.join('\n')).to.contain(
       "You passed the '--typescript' flag but there is no TypeScript blueprint available."
     );
+  });
+
+  it('does not generate typescript when the `--no-typescript` flag, even if a typescript blueprint exists', async function () {
+    await initApp();
+
+    await outputFile(
+      'blueprints/foo/index.js',
+      `module.exports = {
+        shouldTransformTypeScript: true
+      }`
+    );
+
+    await outputFile(
+      'blueprints/foo/files/app/foos/__name__.ts',
+      `export default function <%= camelizedModuleName %>(a: string, b: number): string {
+        return a + b;
+      }`
+    );
+
+    await ember(['generate', 'foo', 'bar', '--no-typescript']);
+    const generated = file('app/foos/bar.js');
+    expect(generated).to.contain('export default function bar(a, b) {');
+  });
+
+  it('does not generate typescript when the `--no-typescript` flag, even if isTypeScriptProject is true', async function () {
+    await initApp();
+
+    await fs.writeJSON('.ember-cli', {
+      isTypeScriptProject: true,
+    });
+
+    await outputFile(
+      'blueprints/foo/index.js',
+      `module.exports = {
+        shouldTransformTypeScript: true
+      }`
+    );
+
+    await outputFile(
+      'blueprints/foo/files/app/foos/__name__.ts',
+      `export default function <%= camelizedModuleName %>(a: string, b: number): string {
+        return a + b;
+      }`
+    );
+
+    await ember(['generate', 'foo', 'bar', '--no-typescript']);
+    const generated = file('app/foos/bar.js');
+    expect(generated).to.contain('export default function bar(a, b) {');
   });
 
   it('can generate classes with decorators and class fields', async function () {
