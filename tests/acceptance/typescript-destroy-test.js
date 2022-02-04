f2ad33278c5f6a0319ee4f275b9426de7bd8c284
Chris Freeman <cfreeman.code@gmail.com>
2022-02-04 15:13:50 -0700

    add `--typescript` flag to `destroy`
    update ember help test

--- tests/acceptance/typescript-destroy-test.js
+++ tests/acceptance/typescript-destroy-test.js
@@ -14,7 +14,7 @@ const chai = require('../chai');
 let expect = chai.expect;
 let file = chai.file;
 
-describe('Acceptance: ember destroy', function () {
+describe('Acceptance: ember destroy with typescript blueprints', function () {
   this.timeout(60000);
   let tmpdir;
 
@@ -87,6 +87,30 @@ describe('Acceptance: ember destroy', function () {
   });
 
   it('deletes TS files generated from typescript blueprints with --typescript', async function () {
+    let commandArgs = ['foo', 'bar'];
+    let files = ['app/foos/bar.ts'];
+    await initApp();
+
+    await fs.outputFile(
+      'blueprints/foo/index.js',
+      `module.exports = {
+        shouldTransformTypeScript: true
+      }`
+    );
+
+    await fs.outputFile(
+      'blueprints/foo/files/app/foos/__name__.ts',
+      "import Ember from 'ember';\n\n" + 'export default Ember.Object.extend({ foo: true });\n'
+    );
+
+    await generate([...commandArgs, '--typescript']);
+    assertFilesExist(files);
+
+    await destroy(commandArgs);
+    assertFilesNotExist(files);
+  });
+
+  it('deletes TS files generated from typescript blueprints when --typescript is passed', async function () {
     let commandArgs = ['foo', 'bar', '--typescript'];
     let files = ['app/foos/bar.ts'];
     await initApp();
@@ -136,5 +160,163 @@ describe('Acceptance: ember destroy', function () {
 
     await destroy(commandArgs);
     assertFilesNotExist(files);
+  });
+
+  it('deletes TS files generated from typescript blueprints when {typescript: true} is present in .ember-cli', async function () {
+    let commandArgs = ['foo', 'bar'];
+    let files = ['app/foos/bar.ts'];
+    await initApp();
+
+    await fs.writeJson('.ember-cli', {
+      typescript: true,
+    });
+
+    await fs.outputFile(
+      'blueprints/foo/index.js',
+      `module.exports = {
+        shouldTransformTypeScript: true
+      }`
+    );
+
+    await fs.outputFile(
+      'blueprints/foo/files/app/foos/__name__.ts',
+      "import Ember from 'ember';\n\n" + 'export default Ember.Object.extend({ foo: true });\n'
+    );
+
+    await generate(commandArgs);
+    assertFilesExist(files);
+
+    await destroy(commandArgs);
+    assertFilesNotExist(files);
+  });
+
+  it('does not delete anything if --typescript is passed and there are no TS files', async function () {
+    let commandArgs = ['foo', 'bar'];
+    let files = ['app/foos/bar.js'];
+    await initApp();
+
+    await fs.outputFile(
+      'blueprints/foo/index.js',
+      `module.exports = {
+        shouldTransformTypeScript: true
+      }`
+    );
+
+    await fs.outputFile(
+      'blueprints/foo/files/app/foos/__name__.ts',
+      "import Ember from 'ember';\n\n" + 'export default Ember.Object.extend({ foo: true });\n'
+    );
+
+    await generate(commandArgs);
+    assertFilesExist(files);
+
+    // console.log('CALLING DESTROY');
+    await destroy([...commandArgs, '--typescript']);
+    assertFilesExist(files);
+  });
+
+  it('does not delete anything if --no-typescript is passed and there are no JS files', async function () {
+    let commandArgs = ['foo', 'bar'];
+    let files = ['app/foos/bar.ts'];
+    await initApp();
+
+    await fs.outputFile(
+      'blueprints/foo/index.js',
+      `module.exports = {
+        shouldTransformTypeScript: true
+      }`
+    );
+
+    await fs.outputFile(
+      'blueprints/foo/files/app/foos/__name__.ts',
+      "import Ember from 'ember';\n\n" + 'export default Ember.Object.extend({ foo: true });\n'
+    );
+
+    await generate([...commandArgs, '--typescript']);
+    assertFilesExist(files);
+
+    await destroy([...commandArgs, '--no-typescript']);
+    assertFilesExist(files);
+  });
+
+  describe('when JS and TS files are present', function () {
+    it('deletes the TS file when --typescript is passed', async function () {
+      let commandArgs = ['foo', 'bar'];
+      let files = ['app/foos/bar.ts', 'app/foos/bar.js'];
+      const [tsFile, jsFile] = files;
+      await initApp();
+
+      await fs.outputFile(
+        'blueprints/foo/index.js',
+        `module.exports = {
+          shouldTransformTypeScript: true
+        }`
+      );
+
+      await fs.outputFile(
+        'blueprints/foo/files/app/foos/__name__.ts',
+        "import Ember from 'ember';\n\n" + 'export default Ember.Object.extend({ foo: true });\n'
+      );
+
+      await generate(commandArgs);
+      await generate([...commandArgs, '--typescript']);
+      assertFilesExist(files);
+
+      await destroy([...commandArgs, '--typescript']);
+      assertFilesNotExist([tsFile]);
+      assertFilesExist([jsFile]);
+    });
+
+    it('deletes the JS file when --no-typescript flag is passed', async function () {
+      let commandArgs = ['foo', 'bar'];
+      let files = ['app/foos/bar.ts', 'app/foos/bar.js'];
+      const [tsFile, jsFile] = files;
+      await initApp();
+
+      await fs.outputFile(
+        'blueprints/foo/index.js',
+        `module.exports = {
+          shouldTransformTypeScript: true
+        }`
+      );
+
+      await fs.outputFile(
+        'blueprints/foo/files/app/foos/__name__.ts',
+        "import Ember from 'ember';\n\n" + 'export default Ember.Object.extend({ foo: true });\n'
+      );
+
+      await generate(commandArgs);
+      await generate([...commandArgs, '--typescript']);
+      assertFilesExist(files);
+
+      await destroy([...commandArgs, '--no-typescript']);
+      assertFilesNotExist([jsFile]);
+      assertFilesExist([tsFile]);
+    });
+
+    it('deletes both files when no flags are passed', async function () {
+      let commandArgs = ['foo', 'bar'];
+      let files = ['app/foos/bar.ts', 'app/foos/bar.js'];
+      await initApp();
+
+      await fs.outputFile(
+        'blueprints/foo/index.js',
+        `module.exports = {
+          shouldTransformTypeScript: true
+        }`
+      );
+
+      await fs.outputFile(
+        'blueprints/foo/files/app/foos/__name__.ts',
+        "import Ember from 'ember';\n\n" + 'export default Ember.Object.extend({ foo: true });\n'
+      );
+
+      await generate(commandArgs);
+      await generate([...commandArgs, '--typescript']);
+      assertFilesExist(files);
+
+      await destroy(commandArgs);
+      assertFilesNotExist(files);
+    });
   });
 });
