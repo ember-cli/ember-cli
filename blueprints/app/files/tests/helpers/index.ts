874a920fc5059bfe0a3ad23986e0e745d38d301f
Simon Ihmig <simon.ihmig@kaliber5.de>
2022-09-24 21:30:08 +0200

    Use new types from ember-qunit

--- blueprints/app/files/tests/helpers/index.ts
+++ blueprints/app/files/tests/helpers/index.ts
@@ -2,16 +2,14 @@ import {
   setupApplicationTest as upstreamSetupApplicationTest,
   setupRenderingTest as upstreamSetupRenderingTest,
   setupTest as upstreamSetupTest,
+  SetupTestOptions,
 } from 'ember-qunit';
 
 // This file exists to provide wrappers around ember-qunit's / ember-mocha's
 // test setup functions. This way, you can easily extend the setup that is
 // needed per test type.
 
-function setupApplicationTest(
-  hooks: Parameters<typeof upstreamSetupApplicationTest>[0],
-  options?: Parameters<typeof upstreamSetupApplicationTest>[1]
-) {
+function setupApplicationTest(hooks: NestedHooks, options?: SetupTestOptions) {
   upstreamSetupApplicationTest(hooks, options);
 
   // Additional setup for application tests can be done here.
@@ -30,19 +28,13 @@ function setupApplicationTest(
   // setupMirage(hooks); // ember-cli-mirage
 }
 
-function setupRenderingTest(
-  hooks: Parameters<typeof upstreamSetupApplicationTest>[0],
-  options?: Parameters<typeof upstreamSetupApplicationTest>[1]
-) {
+function setupRenderingTest(hooks: NestedHooks, options?: SetupTestOptions) {
   upstreamSetupRenderingTest(hooks, options);
 
   // Additional setup for rendering tests can be done here.
 }
 
-function setupTest(
-  hooks: Parameters<typeof upstreamSetupApplicationTest>[0],
-  options?: Parameters<typeof upstreamSetupApplicationTest>[1]
-) {
+function setupTest(hooks: NestedHooks, options?: SetupTestOptions) {
   upstreamSetupTest(hooks, options);
 
   // Additional setup for unit tests can be done here.
