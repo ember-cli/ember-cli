dc8fa73f5922becc35756fe316e559a0002965b0
Bert De Block <bert.deblock@bagaar.be>
2023-02-05 16:32:42 +0100

    Remove `app.import` comment in `app` blueprint

--- tests/fixtures/app/embroider/ember-cli-build.js
+++ tests/fixtures/app/embroider/ember-cli-build.js
@@ -7,19 +7,6 @@ module.exports = function (defaults) {
     // Add options here
   });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
-
   const { Webpack } = require('@embroider/webpack');
   return require('@embroider/compat').compatBuild(app, Webpack, {
     skipBabel: [
