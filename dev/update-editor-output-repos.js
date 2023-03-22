dada2dfda8f6a5d61fcb73ba7c69ac3ec9ccb393
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-03-22 13:13:46 -0400

    Pull in changes from https://github.com/ember-cli/ember-cli/commit/54edebd1dba36713f07249630789d57678502477#diff-f897c4033344856f902db7233a51fb295ccec21de6a9429158e4c21ede251fe1

--- dev/update-editor-output-repos.js
+++ dev/update-editor-output-repos.js
@@ -43,29 +43,25 @@ async function updateOnlineEditorRepos() {
 
     let updatedOutputTmpDir = tmp.dirSync();
     console.log(`Running ember ${command} ${name} (for ${VARIANT})`);
-    await execa(
-      EMBER_PATH,
-      [command, name, `--skip-bower`, `--skip-npm`, `--skip-git`, ...(isTypeScript ? ['--typescript'] : [])],
-      {
-        cwd: updatedOutputTmpDir.name,
-        env: {
-          /**
-           * using --typescript triggers npm's peer resolution features,
-           * and since we don't know if the npm package has been released yet,
-           * (and therefor) generate the project using the local ember-cli,
-           * the ember-cli version may not exist yet.
-           *
-           * We need to tell npm to ignore peers and just "let things be".
-           * Especially since we don't actually care about npm running,
-           * and just want the typescript files to generate.
-           *
-           * See this related issue: https://github.com/ember-cli/ember-cli/issues/10045
-           */
-          // eslint-disable-next-line camelcase
-          npm_config_legacy_peer_deps: 'true',
-        },
-      }
-    );
+    await execa(EMBER_PATH, [command, name, `--skip-npm`, `--skip-git`, ...(isTypeScript ? ['--typescript'] : [])], {
+      cwd: updatedOutputTmpDir.name,
+      env: {
+        /**
+         * using --typescript triggers npm's peer resolution features,
+         * and since we don't know if the npm package has been released yet,
+         * (and therefor) generate the project using the local ember-cli,
+         * the ember-cli version may not exist yet.
+         *
+         * We need to tell npm to ignore peers and just "let things be".
+         * Especially since we don't actually care about npm running,
+         * and just want the typescript files to generate.
+         *
+         * See this related issue: https://github.com/ember-cli/ember-cli/issues/10045
+         */
+        // eslint-disable-next-line camelcase
+        npm_config_legacy_peer_deps: 'true',
+      },
+    });
 
     // node_modules is .gitignored, but since we already need to remove package-lock.json due to #10045,
     // we may as well remove node_modules as while we're at it, just in case.
