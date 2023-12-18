85bd5c581e970be404268154740894b13f8f91b5
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-12-18 14:58:27 -0500

    Set a breakpoint when the window is about to unload.

--- blueprints/app/files/tests/test-helper.ts
+++ blueprints/app/files/tests/test-helper.ts
@@ -5,6 +5,14 @@ import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 
+window.addEventListener('beforeunload', function() {
+  console.error('Window is about to unload. This means some test or code has tried to either set window.location or submit a form without "event.preventDefault()". Please use a mock window, window service, preventDefault, or some other technique to prevent navigation during testing');
+
+	// eslint-disable-next-line no-debugger
+	debugger;
+});
+
+
 setApplication(Application.create(config.APP));
 
 setup(QUnit.assert);
