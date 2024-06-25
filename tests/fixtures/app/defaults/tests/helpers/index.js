e4d0d38f7394f203a4f1f77690ac86628141e7b6
ijlee2 <ijlee2@users.noreply.github.com>
2024-06-25 18:52:20 +0200

    Specified the locale in setupIntl() (reflected the change in ember-intl@v7)

--- tests/fixtures/app/defaults/tests/helpers/index.js
+++ tests/fixtures/app/defaults/tests/helpers/index.js
@@ -23,7 +23,7 @@ function setupApplicationTest(hooks, options) {
   // This is also a good place to call test setup functions coming
   // from other addons:
   //
-  // setupIntl(hooks); // ember-intl
+  // setupIntl(hooks, 'en-us'); // ember-intl
   // setupMirage(hooks); // ember-cli-mirage
 }
 
