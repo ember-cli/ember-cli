0de03611a611006db60ce083229ae042118610ec
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-10-13 10:49:19 -0400

    Fix prettier config

--- blueprints/app/files/.prettierrc.js
+++ blueprints/app/files/.prettierrc.js
@@ -1,11 +1,19 @@
 'use strict';
 
 module.exports = {
+  plugins: ['prettier-plugin-ember-template-tag'],
   overrides: [
     {
-      files: '*.{js,ts,mjs,mts,cjs,cts,gjs,gts}',
+      files: '*.{js,ts,mjs,mts,cjs,cts}',
       options: {
         singleQuote: true,
+      },
+    },
+    {
+      files: '*.{gjs,gts}',
+      options: {
+        singleQuote: true,
+        templateSingleQuote: false,
       },
     },
   ],
