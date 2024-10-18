a2defef8373d5182f8834cae4156b0449c74f953
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-10-18 18:41:51 -0400

    wip

--- blueprints/app/files/_ts_eslint.config.mjs
+++ blueprints/app/files/_ts_eslint.config.mjs
@@ -1,3 +1,17 @@
+/**
+ * Debugging:
+ *   https://eslint.org/docs/latest/use/configure/debug
+ *  ----------------------------------------------------
+ *
+ *   Print a file's calculated configuration
+ *
+ *     npx eslint --print-config path/to/file.js
+ *
+ *   Inspecting the config
+ *
+ *     npx eslint --inspect-config
+ *
+ */
 import globals from 'globals';
 import js from '@eslint/js';
 
@@ -35,10 +49,10 @@ const parserOptions = {
 
 export default ts.config(
   js.configs.recommended,
+  ember.configs.base,
+  ember.configs.gjs,
+  ember.configs.gts,
   prettier,
-  ember.base,
-  ember.gjs,
-  ember.gts,
   /**
    * Ignores must be in their own object
    * https://eslint.org/docs/latest/use/configure/ignore
@@ -70,11 +84,19 @@ export default ts.config(
     },
   },
   {
-    files: ['**/*.{ts,gts}'],
+    files: ['**/*.ts'],
     languageOptions: {
       parserOptions: parserOptions.esm.ts,
     },
-    extends: [...ts.configs.strictTypeChecked],
+    extends: [ember.configs.base, ...ts.configs.recommendedTypeChecked],
+  },
+  {
+    files: ['**/*.gts'],
+    languageOptions: {
+      parser: ember.parser,
+      parserOptions: parserOptions.esm.ts,
+    },
+    extends: [...ts.configs.recommendedTypeChecked, ember.configs.gts],
   },
   {
     files: ['tests/**/*-test.{js,gjs,ts,gts}'],
