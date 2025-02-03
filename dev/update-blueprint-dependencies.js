666aff3c0de26b0f842d7e2d2e02bdb609309906
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2025-02-03 11:28:01 +0100

    Add support for the `@ember-data/*` packages in the update-blueprint-dependencies.js script
    EmberData is no longer in a lockstep version cadence with the ember-source and ember-cli projects. As a result, it's possible that no new ember-data stable release is out when ember-cli bumps the stable deps. This means that the `@ember-data/*` package.json files of the blueprints are published with their `-beta.` versions.
    
    This updates the script so that it uses the same version that we use for the main `ember-data` package.

--- dev/update-blueprint-dependencies.js
+++ dev/update-blueprint-dependencies.js
@@ -100,6 +100,10 @@ async function latestVersion(packageName, semverRange) {
       version: semverRange,
     };
 
+    if (isEmberDataPackage(packageName) && OPTIONS['ember-data']) {
+      options.version = OPTIONS['ember-data'];
+    }
+
     if (OPTIONS[packageName]) {
       options.version = OPTIONS[packageName];
     }
@@ -187,6 +191,10 @@ if (module === require.main) {
     process.exitCode = 1;
     return;
   }
+}
+
+function isEmberDataPackage(packageName) {
+  return packageName.includes('ember-data');
 }
 
 module.exports = { PACKAGE_FILES, updateDependencies };
