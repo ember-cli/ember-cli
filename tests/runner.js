3e742da4ba10012fa15b0c0f5a64cf98cfebecba
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-03-11 14:44:43 -0400

    DEBUG

--- tests/runner.js
+++ tests/runner.js
@@ -53,12 +53,10 @@ function runMocha() {
     console.timeEnd('Mocha Tests Running Time');
 
     console.log(`[Mocha Debug]`, failures, typeof failures);
-    if (failures > 0) {
-      console.info(`Exiting with status ${failures}`);
-      // On Windows, process.exit at all causes exit code 1
-      // eslint-disable-next-line n/no-process-exit
-      process.exit(failures);
-    }
+    console.info(`Exiting with status ${failures}`);
+
+    // eslint-disable-next-line n/no-process-exit
+    process.exit(failures > 0 ? 1 : 0);
   });
 }
 
@@ -66,14 +64,4 @@ process.on('exit', (...args) => {
   console.info(`on:exit`, ...args);
 });
 
-Promise.resolve()
-  .then(() => runMocha())
-  .then(() => console.info('Success'))
-  .catch((error) => {
-    console.info(`An error occurred!`);
-    console.error(error);
-    console.error(error.stack);
-
-    // eslint-disable-next-line n/no-process-exit
-    process.exit(1);
-  });
+runMocha();
