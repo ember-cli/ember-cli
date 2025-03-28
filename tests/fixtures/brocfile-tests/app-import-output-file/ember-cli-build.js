11da77c0dc4a3a28b096edc1cab291b25bd746d8
Chris Manson <chris@manson.ie>
2025-03-28 15:54:24 +0000

    fix experiments in slow tests

--- tests/fixtures/brocfile-tests/app-import-output-file/ember-cli-build.js
+++ tests/fixtures/brocfile-tests/app-import-output-file/ember-cli-build.js
@@ -1,5 +1,5 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-const { isExperimentEnabled } = require('ember-cli/lib/experiments');
+const { isExperimentEnabled } = require('@ember/blueprint-model/utilities/experiments');
 
 module.exports = function (defaults) {
   var app = new EmberApp(defaults, {});
