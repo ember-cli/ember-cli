f7b252c843e6806695183e747cac13a863e24c6c
Chris Manson <chris@manson.ie>
2025-03-28 15:54:24 +0000

    fix experiments in slow tests

--- tests/fixtures/addon/with-blueprint-override/blueprints/component/index.js
+++ tests/fixtures/addon/with-blueprint-override/blueprints/component/index.js
@@ -1,4 +1,4 @@
-const { isExperimentEnabled } = require('../../../../lib/experiments');
+const { isExperimentEnabled } = require('@ember/blueprint-model/utilities/experiments');
 const path = require('path');
 
 
