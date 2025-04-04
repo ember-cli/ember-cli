045d4114ca4d60fd5e612cc93b5f1098ef45981f
Katie Gengler <katie@kmg.io>
2025-04-04 15:44:55 -0400

    Add configuration to ember-cli-build to opt new projects out of the deprecated behavior of the Store class extending EmberObject

--- blueprints/app/files/ember-cli-build.js
+++ blueprints/app/files/ember-cli-build.js
@@ -4,7 +4,16 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    <% if (typescript) {%>'ember-cli-babel': { enableTypeScriptTransform: true },
+    <% if (emberData) {%>emberData: {
+      deprecations: {
+        // New projects can safely leave this deprecation disabled.
+        // If upgrading, to opt-into the deprecated behavior, set this to true and then follow:
+        // https://deprecations.emberjs.com/id/ember-data-deprecate-store-extends-ember-object
+        // before upgrading to Ember Data 6.0
+        DEPRECATE_STORE_EXTENDS_EMBER_OBJECT: false,
+      },
+    },
+    <% } %><% if (typescript) {%>'ember-cli-babel': { enableTypeScriptTransform: true },
 
     <% } %>// Add options here
   });
--- tests/fixtures/app/typescript-embroider-no-ember-data/ember-cli-build.js
+++ tests/fixtures/app/typescript-embroider-no-ember-data/ember-cli-build.js
@@ -4,15 +4,6 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    emberData: {
-      deprecations: {
-        // New projects can safely leave this deprecation disabled.
-        // If upgrading, to opt-into the deprecated behavior, set this to true and then follow:
-        // https://deprecations.emberjs.com/id/ember-data-deprecate-store-extends-ember-object
-        // before upgrading to Ember Data 6.0
-        DEPRECATE_STORE_EXTENDS_EMBER_OBJECT: false,
-      },
-    },
     'ember-cli-babel': { enableTypeScriptTransform: true },
 
     // Add options here
