8192f96bd24f1e7af59b26bc2c2866b41da55300
Florian Pichler <fp@ylk.gd>
2025-09-01 10:47:32 +0200

    Group Embroider Experiment related tests

--- tests/acceptance/new-test.js
+++ tests/acceptance/new-test.js
@@ -365,36 +365,38 @@ describe('Acceptance: ember new', function () {
     expect(pkgJson.name).to.equal('foo', 'uses app name for package name');
   });
 
-  if (!isExperimentEnabled('CLASSIC')) {
-    it('embroider experiment creates the correct files', async function () {
-      let ORIGINAL_PROCESS_ENV = process.env.EMBER_CLI_EMBROIDER;
-      process.env['EMBER_CLI_EMBROIDER'] = 'true';
-      await ember(['new', 'foo', '--skip-npm', '--skip-git']);
+  describe('Experiment: Embroider', function () {
+    if (!isExperimentEnabled('CLASSIC')) {
+      it('embroider experiment creates the correct files', async function () {
+        let ORIGINAL_PROCESS_ENV = process.env.EMBER_CLI_EMBROIDER;
+        process.env['EMBER_CLI_EMBROIDER'] = 'true';
+        await ember(['new', 'foo', '--skip-npm', '--skip-git']);
 
-      if (ORIGINAL_PROCESS_ENV === undefined) {
-        delete process.env['EMBER_CLI_EMBROIDER'];
-      } else {
-        process.env['EMBER_CLI_EMBROIDER'] = ORIGINAL_PROCESS_ENV;
+        if (ORIGINAL_PROCESS_ENV === undefined) {
+          delete process.env['EMBER_CLI_EMBROIDER'];
+        } else {
+          process.env['EMBER_CLI_EMBROIDER'] = ORIGINAL_PROCESS_ENV;
+        }
+
+        let pkgJson = fs.readJsonSync('package.json');
+        expect(pkgJson.devDependencies['@embroider/compat']).to.exist;
+        expect(pkgJson.devDependencies['@embroider/core']).to.exist;
+        expect(pkgJson.devDependencies['@embroider/webpack']).to.exist;
+      });
+    }
+
+    it('embroider enabled with --embroider', async function () {
+      if (DEPRECATIONS.EMBROIDER.isRemoved) {
+        this.skip();
       }
+
+      await ember(['new', 'foo', '--skip-npm', '--skip-git', '--embroider']);
 
       let pkgJson = fs.readJsonSync('package.json');
       expect(pkgJson.devDependencies['@embroider/compat']).to.exist;
       expect(pkgJson.devDependencies['@embroider/core']).to.exist;
       expect(pkgJson.devDependencies['@embroider/webpack']).to.exist;
     });
-  }
-
-  it('embroider enabled with --embroider', async function () {
-    if (DEPRECATIONS.EMBROIDER.isRemoved) {
-      this.skip();
-    }
-
-    await ember(['new', 'foo', '--skip-npm', '--skip-git', '--embroider']);
-
-    let pkgJson = fs.readJsonSync('package.json');
-    expect(pkgJson.devDependencies['@embroider/compat']).to.exist;
-    expect(pkgJson.devDependencies['@embroider/core']).to.exist;
-    expect(pkgJson.devDependencies['@embroider/webpack']).to.exist;
   });
 
   describe('verify fixtures', function () {
