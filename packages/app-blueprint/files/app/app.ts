250bce01182af5e1ea5d71fd49f6052e07ec886f
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-09-06 11:45:04 -0400

    Add import from ember-data breakage

--- packages/app-blueprint/files/app/app.ts
+++ packages/app-blueprint/files/app/app.ts
@@ -1,4 +1,5 @@
-import Application from '@ember/application';
+<% if (emberData) { %>import '@warp-drive/ember/install';
+<% } %> import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from '<%= modulePrefix %>/config/environment';
