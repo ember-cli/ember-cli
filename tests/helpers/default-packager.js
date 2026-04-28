7049ac0e4952455ebc0a2fd7b52087e39998f462
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-04-28 10:30:15 -0400

    Cleanup for RFC 1029

--- tests/helpers/default-packager.js
+++ tests/helpers/default-packager.js
@@ -77,14 +77,8 @@ const DEFAULT_VENDOR = {
   'ember-cli': {
     'app-boot.js': '!!!',
     'app-config.js': 'SPARTA',
-    'app-prefix.js': 'THIS',
-    'app-suffix.js': 'IS',
     'test-support-prefix.js': 'If a clod be washed away by the sea,',
     'test-support-suffix.js': 'Europe is the less.',
-    'tests-prefix.js': 'As well as if a promontory were.',
-    'tests-suffix.js': 'As well as if a manor of thine own',
-    'vendor-prefix.js': 'HELLO',
-    'vendor-suffix.js': '!',
   },
   'ember-cli-shims': {
     'app-shims.js': 'L',
