5ce7fe218d4d39d4181d63dba9fefd3ce0061e04
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2026-07-30 10:02:17 +0200

    Update the classic app to Glint v2

--- tests/acceptance/new-test.js
+++ tests/acceptance/new-test.js
@@ -304,15 +304,11 @@ describe('Acceptance: ember new', function () {
       expect(file('app/index.html')).to.contain('<html>');
 
       // no TypeScript files
-      [
-        'tsconfig.json',
-        'tsconfig.declarations.json',
-        'app/config/environment.d.ts',
-        'types/global.d.ts',
-        'types/foo/index.d.ts',
-      ].forEach((filePath) => {
-        expect(file(filePath)).to.not.exist;
-      });
+      ['tsconfig.json', 'tsconfig.declarations.json', 'app/config/environment.d.ts', 'types/foo/index.d.ts'].forEach(
+        (filePath) => {
+          expect(file(filePath)).to.not.exist;
+        }
+      );
     });
 
     it('app + npm + !welcome', async function () {
@@ -431,13 +427,7 @@ describe('Acceptance: ember new', function () {
       }
 
       // check fixtures
-      [
-        '.ember-cli',
-        'tests/helpers/index.ts',
-        'tsconfig.json',
-        'app/config/environment.d.ts',
-        'types/global.d.ts',
-      ].forEach((filePath) => {
+      ['.ember-cli', 'tests/helpers/index.ts', 'tsconfig.json', 'app/config/environment.d.ts'].forEach((filePath) => {
         checkFile(filePath, path.join(__dirname, '../fixtures', fixturePath, filePath));
       });
       checkFileWithJSONReplacement(
